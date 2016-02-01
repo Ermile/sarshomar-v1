834d1b6cddec282ee46370727a5c4eb9efea3d8e
baravak <itb.baravak@gmail.com>
2016-02-01 11:25:52 +0330

    gitignore & php webhooks bugfix

--- bot/php/index.php
+++ bot/php/index.php
@@ -1,4 +1,4 @@
 <?php
 $message = json_decode(file_get_contents('php://input'), true);
-file_put_contents('telegram.json', $message, FILE_APPEND);
+file_put_contents('telegram.json', json_encode($message));
 ?>
