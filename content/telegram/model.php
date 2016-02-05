214ea2fbe9be2fa27a9c9cb84810ebabda6439c5
baravak <itb.baravak@gmail.com>
2016-02-05 20:19:54 +0330

    dynamic user sender

--- content/telegram/model.php
+++ content/telegram/model.php
@@ -24,7 +24,8 @@ class model extends \lib\model
 		
 
 		$text = $telegram_hooks_get->message->text;
-		$x = $bot->sendMessage(58164083, "question : $text", 'Markdown', false, null, $keyboard);
+		$chat_id = $telegram_hooks_get->chat->id;
+		$x = $bot->sendMessage($chat_id, "question : $text", 'Markdown', false, null, $keyboard);
 
 		return json_decode(json_encode($x));
 
