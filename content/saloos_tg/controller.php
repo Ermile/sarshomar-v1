f0caffdbfffe5d30b57f464fa9808df00c2c5cde
Javad <J.Evazzadeh@live.com>
2016-04-24 13:50:44 +0430

    create keyboard

--- content/saloos_tg/controller.php
+++ content/saloos_tg/controller.php
@@ -16,10 +16,33 @@ class controller extends \lib\mvc\controller
 
 	static function tg_handle()
 	{
+		// run hook and get it
 		$hook    = \lib\utility\social\tg::hook(true);
+		// extract chat_id if not exist return false
 		$chat_id = self::tg_chat($hook);
-		$data = ['chat_id' => $chat_id, 'text' => 'test 1'];
-		var_dump($data);
+		if(!$chat_id)
+		{
+			return 'chat id is not exist!';
+		}
+
+		// create keyboard
+		$keyboard =
+		[
+			'ReplyKeyboardMarkup' =>
+			[
+				'keyboard' =>
+				[
+					["A", "B"]
+				]
+			]
+		];
+		// generate data
+		$data =
+		[
+			'chat_id'      => $chat_id,
+			'text'         => 'test message send from sarshomar!',
+			'reply_markup' => $keyboard
+		];
 
 		$result = \lib\utility\social\tg::sendMessage($data);
 		var_dump($result);
