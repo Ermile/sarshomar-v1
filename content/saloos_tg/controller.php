b0dd95cbfe331a8cf2ba9c0b5d0af3cf57217971
Javad <J.Evazzadeh@live.com>
2016-04-24 19:34:05 +0430

    add another inline btn

--- content/saloos_tg/controller.php
+++ content/saloos_tg/controller.php
@@ -141,6 +141,14 @@ class controller extends \lib\mvc\controller
 								'text' => '>',
 								'callback_data' => 'go_right'
 							]
+							[
+								'text' => 'open google.com',
+								'url' => 'google.com'
+							],
+							[
+								'text' => 'search \'test\' inline',
+								'switch_inline_query' => 'test'
+							],
 						]
 
 
