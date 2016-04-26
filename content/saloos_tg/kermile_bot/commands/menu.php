c3ca43e91be6c3531710715d35d54e7ce1736e48
Javad <J.Evazzadeh@live.com>
2016-04-26 19:31:24 +0430

    update kermile bot

---
+++ content/saloos_tg/kermile_bot/commands/menu.php
@@ -0,0 +1,191 @@
+<?php
+namespace content\saloos_tg\kermile_bot\commands;
+// use telegram class as bot
+use \lib\utility\telegram\tg as bot;
+
+class menu
+{
+	public static $return = true;
+
+	public static function exec($_cmd)
+	{
+		$response = null;
+		switch ($_cmd['command'])
+		{
+			case 'menu':
+			case 'منو':
+				$response = self::menu0();
+				break;
+
+			case 'loc':
+			case 'موقعیت':
+				$response = self::menu_loc();
+				break;
+
+
+			case 'inline':
+			case 'اینلاین':
+				$response = self::menu_inline();
+				break;
+
+			case 'main':
+			case 'mainmenu':
+			case 'menu0':
+			case 'منو۰':
+				$response = self::menu_main();
+				break;
+
+			case 'return':
+			case 'بازگشت':
+				$response = self::returnBtn();
+				break;
+
+			default:
+				break;
+		}
+
+		// automatically add return to end of keyboard
+		if(self::$return)
+		{
+			// if has keyboard
+			if(isset($response['replyMarkup']['keyboard']))
+			{
+				$response['replyMarkup']['keyboard'][] = ['بازگشت'];
+			}
+		}
+
+		return $response;
+	}
+
+
+	/**
+	 * return menu
+	 * @return [type] [description]
+	 */
+	public static function returnBtn()
+	{
+		$result['text'] = 'بازگشت به منوی قبلی'."\r\n";
+		$result['text'] .= 'مثلا برگشتیم'."\r\n";
+		$result['replyMarkup'] =
+		[
+			'keyboard' =>
+			[
+					["خوب"],
+					["بد"],
+			],
+		];
+		return $result;
+	}
+
+
+	/**
+	 * return menu
+	 * @return [type] [description]
+	 */
+	public static function menu0()
+	{
+		$result['text'] = 'منوی آزمایشی'."\r\n";
+		$result['replyMarkup'] =
+		[
+			'keyboard' =>
+			[
+					["سلام"],
+					["خوبی"],
+					["خوب", "بد"],
+			],
+		];
+		return $result;
+	}
+
+
+	/**
+	 * return menu
+	 * @return [type] [description]
+	 */
+	public static function menu_main()
+	{
+		// disable return from main menu
+		self::$return          = false;
+		$result['text']        = 'منوی اصلی'."\r\n";
+		$result['replyMarkup'] =
+		[
+			'keyboard' =>
+			[
+					["منوی آزمایش"],
+					["نظرسنجی های من"],
+					["مقالات"],
+					["پروفایل"],
+			],
+		];
+		return $result;
+	}
+
+
+	/**
+	 * return menu
+	 * @return [type] [description]
+	 */
+	public static function menu_loc()
+	{
+		$result['text']        = 'منوی موقعیت'."\r\n";
+		$result['replyMarkup'] =
+		[
+			'keyboard' =>
+			[
+				[
+					[
+						'text'            => 'تقاضای شماره تلفن',
+						'request_contact' => true
+					],
+					[
+						'text'             => 'تقاضای آدرس',
+						'request_location' => true
+					]
+				]
+			]
+		];
+		return $result;
+	}
+
+
+	/**
+	 * return menu
+	 * @return [type] [description]
+	 */
+	public static function menu_inline()
+	{
+		$result['text']        = 'منوی اینلاین آزمایشی'."\r\n";
+		$result['replyMarkup'] =
+		[
+			'inline_keyboard' =>
+			[
+				[
+					[
+						'text'          => '<',
+						'callback_data' => 'go_left'
+					],
+					[
+						'text'          => '^',
+						'callback_data' => 'go_up'
+					],
+					[
+						'text'          => '>',
+						'callback_data' => 'go_right'
+					],
+				],
+				[
+					[
+						'text' => 'open google.com',
+						'url'  => 'google.com'
+					],
+					[
+						'text'                => 'search \'test\' inline',
+						'switch_inline_query' => 'test'
+					],
+				]
+			],
+		];
+		return $result;
+	}
+}
+?>
