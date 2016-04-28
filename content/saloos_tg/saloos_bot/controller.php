091176cab439f1a921bc30333631ef5e1bdfe651
Javad <J.Evazzadeh@live.com>
2016-04-28 16:29:28 +0430

    update for saloosbot

--- content/saloos_tg/saloos_bot/controller.php
+++ content/saloos_tg/saloos_bot/controller.php
@@ -18,6 +18,17 @@ class controller extends \lib\mvc\controller
 			bot::$api_key   = '164997863:AAFC3nUcujDzpGq-9ZgzAbZKbCJpnd0FWFY';
 			// bot::$cmdFolder = '\\'. __NAMESPACE__ .'\commands\\';
 			// bot::$useSample = true;
+			$txt_about      = "آرامش و آسایش به همراه لذیذترین غذاهای محلی گیلان در رستوران مجلل چاچوق تجربه کنید.\r\n\n";
+			$txt_about      .= "هتل بوتیک تجاری آرامیس با ۸۴ واحد اقامتی شامل اتاق و سوئیت مجلل و مدرن پذیرای مهمانان عزیز می باشد.\r\n";
+			$txt_about      .= "این رستوران در یک طبقه مجزا و به ظرفیت ۴۵۰ نفر طراحی شده است.";
+
+			bot::$fill      =
+			[
+				'name'     => 'ارمایل',
+				'fullName' => 'هتل بین المللی آرامیس تهران',
+				'about'    => $txt_about,
+				'type'     => 'هتل',
+			];
 			$result         = bot::handle();
 
 			if(\lib\utility\option::get('telegram', 'meta', 'debug'))
