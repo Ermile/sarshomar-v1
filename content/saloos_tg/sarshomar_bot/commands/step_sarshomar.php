0e8a5303e167bc7ee7339dcc4e8aa4642af97d14
Javad <J.Evazzadeh@live.com>
2016-05-26 15:43:19 +0430

    add pashokh

--- content/saloos_tg/sarshomar_bot/commands/step_sarshomar.php
+++ content/saloos_tg/sarshomar_bot/commands/step_sarshomar.php
@@ -378,7 +378,7 @@ class step_sarshomar
 		$result       = \lib\db\polls::getResult($_question_id, 'count', 'txt');
 		arsort($result);
 		$result_count = array_sum($result);
-		$output       = $_question." ";
+		$output       = "پاسخ سوال "$_question." ";
 		$output       .= "(". $result_count. " نفر)\n";
 
 
