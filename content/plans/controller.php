bed2a3d5c06cb37b764807e3b33bbdb1a2e9bcf2
Javad <J.Evazzadeh@live.com>
2016-07-19 20:14:15 +0430

    add plans page

---
+++ content/plans/controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace content\plans;
+use \lib\saloos;
+
+class controller extends \mvc\controller
+{
+
+}
+?>
---
+++ content/plans/view.php
@@ -0,0 +1,17 @@
+<?php
+namespace content\plans;
+
+class view extends \mvc\view
+{
+	function config()
+	{
+		// $this->include->css_ermile   = false;
+		$this->include->js         = false;
+		$this->include->js_main    = false;
+		$this->include->chart      = false;
+		// $this->include->css_ermile = false;
+		$this->include->css        = false;
+
+	}
+}
+?>
