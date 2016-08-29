370cff7c2052f67ebe2a9081ac3bb20ad03840eb
Ahmad <ahmadkarimi1991@gmail.com>
2016-08-29 19:16:24 +0430

    change tooltip share to false

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -25,7 +25,7 @@ $(function() {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' + '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
             footerFormat: '</table>',
-            shared: true,
+            shared: false,
             useHTML: true
         },
         plotOptions: {
