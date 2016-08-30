db1652d7d352c3c34bbddd2d3893d423e15b01f7
Ahmad <ahmadkarimi1991@gmail.com>
2016-08-30 18:18:53 +0430

    exporting removed, subtitle removed, credits removed, yAxis max removed, xAxis title removed

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -1,22 +1,34 @@
+var _max;
 $(function() {
     $('.top-chart').highcharts({
         chart: {
-            type: 'column'
+            type: 'column',
+            // events: {
+            //     load: function() {
+            //         var check = $('.top-chart').highcharts();
+            //         check.yAxis[0].setExtremes(null, check.axes[1].dataMax);
+            //     }
+            // }
+        },
+        exporting: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
         },
         title: {
             text: 'کدام روش را برای رسیدن به وزن ایده آل مناسبتر میدانید؟'
         },
-        subtitle: {
-            text: 'منبع: sarshomar.com'
-        },
         xAxis: {
+            labels: {
+                enabled: false
+            },
             categories: [
                 'روش های رسیدن به وزن ایده آل'
             ]
         },
         yAxis: {
             min: 0,
-            max: 100,
             title: {
                 text: 'درصد'
             }
@@ -36,66 +48,62 @@ $(function() {
         },
         series: [{
             name: 'ورزش و رژیم همزمان',
-            data: [60]
+            data: [30]
         }, {
-        	name: 'ورزش منظم',
-        	data: [20]
+            name: 'ورزش منظم',
+            data: [20]
         }, {
-        	name: 'عمل جراحی',
-        	data: [20]
+            name: 'عمل جراحی',
+            data: [20]
         }, {
-        	name: 'داروهای لاغری',
-        	data: [0]
+            name: 'داروهای لاغری',
+            data: [0]
         }]
     });
 
-    $.ajax({
-        type : 'post',
-        url : '/',
-        data :{'random' : 'random'},
-        success : function (data) {
-            console.log(data);
-            $('.random-chart').highcharts({
-                chart: {
-                    type: 'column'
-                },
-                title: {
-                    text: data.title
-                },
-                subtitle: {
-                    text: 'منبع: sarshomar.com'
-                },
-                xAxis: {
-                    categories: [
-                        data.title
-                    ]
-                },
-                yAxis: {
-                    min: 0,
-                    max: 100,
-                    title: {
-                        text: 'درصد'
-                    }
-                },
-                tooltip: {
-                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' + '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
-                    footerFormat: '</table>',
-                    shared: false,
-                    useHTML: true
-                },
-                plotOptions: {
-                    column: {
-                        pointPadding: 0.2,
-                        borderWidth: 0
-                    }
-                },
-                series: data.result
-            });
-
-        }
-    });
-
-
-
+    // $.ajax({
+    //     type : 'post',
+    //     url : '/',
+    //     data :{'random' : 'random'},
+    //     success : function (data) {
+    //         console.log(data);
+    //         $('.random-chart').highcharts({
+    //             chart: {
+    //                 type: 'column'
+    //             },
+    //             title: {
+    //                 text: data.title
+    //             },
+    //             subtitle: {
+    //                 text: 'منبع: sarshomar.com'
+    //             },
+    //             xAxis: {
+    //                 categories: [
+    //                     data.title
+    //                 ]
+    //             },
+    //             yAxis: {
+    //                 min: 0,
+    //                 max: 100,
+    //                 title: {
+    //                     text: 'درصد'
+    //                 }
+    //             },
+    //             tooltip: {
+    //                 headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+    //                 pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' + '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+    //                 footerFormat: '</table>',
+    //                 shared: false,
+    //                 useHTML: true
+    //             },
+    //             plotOptions: {
+    //                 column: {
+    //                     pointPadding: 0.2,
+    //                     borderWidth: 0
+    //                 }
+    //             },
+    //             series: data.result
+    //         });
+    //     }
+    // });
 });
