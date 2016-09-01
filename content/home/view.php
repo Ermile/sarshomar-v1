fa786ca1b695c74fecbd09be46deba813502f402
Ahmad <ahmadkarimi1991@gmail.com>
2016-09-01 16:35:41 +0430

    highcharts added to project

--- content/home/view.php
+++ content/home/view.php
@@ -8,7 +8,7 @@ class view extends \mvc\view
 
 		// $this->include->css_ermile   = false;
 		$this->include->js    = true;
-		// $this->include->chart = true;
+		$this->include->chart = true;
 		if($this->module() === 'home')
 		{
 			$this->include->js_main      = true;
