5d5acfe27b1eebb847ff3f0b84ec54f63e257193
Ahmad <ahmadkarimi1991@gmail.com>
2016-09-25 20:14:34 +0330

    add template files and poll page data functionality

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -1,64 +1,43 @@
-
-  $('#tag-add').keypress(function(e)
-  {
+$('#tag-add').keypress(function(e) {
     // if Enter pressed disallow it and run add func
-    if(e.which == 13) { addTag(); return false; }
-  });
-  $(document).on('click', '#tag-add-btn', function () { addTag(); });
-  $(document).on('click', '#tag-list span i', function ()
-  {
+    if (e.which == 13) { addTag();
+        return false; }
+});
+$(document).on('click', '#tag-add-btn', function() { addTag(); });
+$(document).on('click', '#tag-list span i', function() {
     var span = $(this).parent();
-    $('#sp-tags').val($('#sp-tags').val().replace(span.text()+', ', ''));
+    $('#sp-tags').val($('#sp-tags').val().replace(span.text() + ', ', ''));
     span.remove();
-  });
+});
 
-  $(document).ready(function()
-  {
-
-
+$(document).ready(function() {
     var tagDefault = $('#sp-tags').val();
     $('#tag-list').text('');
-    if(tagDefault)
-    {
-      $.each(tagDefault.split(', '),function(t, item)
-      {
-        if(item.trim())
-          $('#tag-list').append( "<span><i class='fa fa-times'></i>"+item+"</span>" );
-      });
+    if (tagDefault) {
+        $.each(tagDefault.split(', '), function(t, item) {
+            if (item.trim())
+                $('#tag-list').append("<span><i class='fa fa-times'></i>" + item + "</span>");
+        });
     }
+});
 
-  });
-
-
-
-
-function addTag()
-{
-  var tag = $('#tag-add');
-  var newTag = tag.val().trim();
-  if(newTag)
-  {
-
-    var exist = false;
-    $.each($('#sp-tags').val().split(', '),function(t, item)
-    {
-      if(item == newTag) {exist = t+1;}
-    });
-
-    if(exist)
-    {
-      existEl = $("#tag-list span:nth-child("+exist+")" );
-      bg = existEl.css('background-color');
-      existEl.css('background-color', '#ddd');
-      setTimeout(function () { existEl.css("background-color",bg) }, 500);
+function addTag() {
+    var tag = $('#tag-add');
+    var newTag = tag.val().trim();
+    if (newTag) {
+        var exist = false;
+        $.each($('#sp-tags').val().split(', '), function(t, item) {
+            if (item == newTag) { exist = t + 1; }
+        });
+        if (exist) {
+            existEl = $("#tag-list span:nth-child(" + exist + ")");
+            bg = existEl.css('background-color');
+            existEl.css('background-color', '#ddd');
+            setTimeout(function() { existEl.css("background-color", bg) }, 500);
+        } else {
+            $('#tag-list').append("<span><i class='fa fa-times'></i>" + newTag + "</span>");
+            $('#sp-tags').val($('#sp-tags').val() + newTag + ', ');
+        }
     }
-    else
-    {
-      $('#tag-list').append( "<span><i class='fa fa-times'></i>"+newTag+"</span>" );
-      $('#sp-tags').val( $('#sp-tags').val() + newTag+', ' );
-    }
-  }
-  tag.val('');
+    tag.val('');
 }
-
-
