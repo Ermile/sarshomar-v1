4bda17105d97bc2067fdf7d9eb8567c637375b80
Reza Mohiti <rm.biqarar@gmail.com>
2016-09-30 09:19:57 +0330

    add all filter profile

--- content_u/profile/model.php
+++ content_u/profile/model.php
@@ -23,22 +23,57 @@ class model extends \mvc\model
 	 */
 	public function post_profile()
 	{
+		$displayname       = utility::post("displayname");
+		// $mobile            = utility::post("mobile");
 		$gender            = utility::post("gender");
 		$marrital_status   = utility::post("marrital_status");
+		$parental_status   = utility::post("parental_status");
+		$exercise_habits   = utility::post("exercise_habits");
+		$employment_status = utility::post("employment_status");
+		$business_owner    = utility::post("business_owner");
+		$industry          = utility::post("industry");
+		$devices_owned     = utility::post("devices_owned");
+		$internet_usage    = utility::post("internet_usage");
 		$birthdate         = utility::post("birthdate");
+		$range             = utility::post("range");
 		$graduation        = utility::post("graduation");
+		$course            = utility::post("course");
+		$country_birth     = utility::post("country_birth");
+		$country           = utility::post("country");
+		$province_birth    = utility::post("province_birth");
+		$province          = utility::post("province");
+		$birthcity         = utility::post("birthcity");
 		$city              = utility::post("city");
+		$favorites         = utility::post("favorites");
 		$language          = utility::post("language");
 
-		// $age               = self::get_age($birthdate);
+		$age               = self::get_age($birthdate);
 
 		$args =
 		[
+			'displayname'       => $displayname,
+			// 'mobile'            => $mobile,
 			'gender'            => $gender,
 			'marrital_status'   => $marrital_status,
+			'parental_status'   => $parental_status,
+			'exercise_habits'   => $exercise_habits,
+			'employment_status' => $employment_status,
+			'business_owner'    => $business_owner,
+			'industry'          => $industry,
+			'devices_owned'     => $devices_owned,
+			'internet_usage'    => $internet_usage,
 			'birthdate'         => $birthdate,
+			'age'               => $age,
+			'range'             => $range,
 			'graduation'        => $graduation,
+			'course'            => $course,
+			'country_birth'     => $country_birth,
+			'country'           => $country,
+			'province_birth'    => $province_birth,
+			'province'          => $province,
+			'birthcity'         => $birthcity,
 			'city'              => $city,
+			'favorites'         => $favorites,
 			'language'          => $language
 		];
 
