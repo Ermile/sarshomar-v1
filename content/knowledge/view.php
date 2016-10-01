6dbfb2176a33cab7206b9ffefc615f66085ea113
Reza Mohiti <rm.biqarar@gmail.com>
2016-10-01 13:40:54 +0330

    poll page load css bug fix

--- content/knowledge/view.php
+++ content/knowledge/view.php
@@ -5,13 +5,28 @@ class view extends \mvc\view
 {
 	function config()
 	{
-		// $this->include->css_ermile   = false;
-		$this->include->js         = false;
-		$this->include->js_main    = false;
-		$this->include->chart      = false;
-		// $this->include->css_ermile = false;
-		$this->include->css        = false;
+			// $this->include->css_ermile   = false;
+		$this->include->js    = true;
+		$this->include->chart = true;
+		if($this->module() === 'home')
+		{
+			$this->include->js_main      = true;
+		}
+		// $this->data->chart      = \lib\db\polls::getResult(3, 'count', 'txt');
+		$post = $this->model()->get_posts();
+		if(isset($post['id']))
+		{
+			$post_id = $post['id'];
+			$this->data->chart      = \lib\db\stat_polls::get_result($post_id);
+			$this->data->chart_type = 'column';
+		}
+		else
+		{
+			$this->data->result = $this->model()->random_result();
+		}
 
+		$this->data->stat = T_(":number Questions answered", ["number"=>\lib\db\stat_polls::get_sarshomar_total_answered()]);
+		$this->include->fontawesome = true;
 	}
 
 
