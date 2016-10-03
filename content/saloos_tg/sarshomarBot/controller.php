cc55ee5a94cf1fd7585bad012f911435f8a95b9a
Javad <J.Evazzadeh@live.com>
2016-10-03 16:12:11 +0330

    add sarshomarBot folder for backup

---
+++ content/saloos_tg/sarshomarBot/controller.php
@@ -0,0 +1,39 @@
+<?php
+namespace content\saloos_tg\sarshomar_bot;
+// use telegram class as bot
+use \lib\telegram\tg as bot;
+
+class controller extends \lib\mvc\controller
+{
+	/**
+	 * allow telegram to access to this location
+	 * to send response to our server
+	 * @return [type] [description]
+	 */
+	function _route()
+	{
+		$myhook = 'saloos_tg/sarshomar_bot/'.\lib\utility\option::get('telegram', 'meta', 'hookFolder');
+		if($this->url('path') == $myhook)
+		{
+			bot::$api_key     = '186535040:AAGKVOlmlpA4wU0Vjv0-s93w_o2aB3n0xKE';
+			bot::$name        = 'sarshomar_bot';
+			bot::$cmdFolder   = '\\'. __NAMESPACE__ .'\commands\\';
+			bot::$defaultText = T_('Undefined');
+			bot::$defaultMenu = commands\menu::main(true);
+			bot::$fill        =
+			[
+				'name'     => T_('Sarshomar'),
+				'fullName' => T_('Sarshomar'),
+				// 'about'    => $txt_about,
+			];
+			$result           = bot::run(true);
+
+			if(\lib\utility\option::get('telegram', 'meta', 'debug'))
+			{
+				var_dump($result);
+			}
+			exit();
+		}
+	}
+}
+?>
