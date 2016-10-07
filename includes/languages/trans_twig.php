fdf01e1b69d8d1f60909bca0104f200376358c44
Ahmad <ahmadkarimi1991@gmail.com>
2016-10-07 15:29:27 +0330

    add menu to poll add page

--- includes/languages/trans_twig.php
+++ includes/languages/trans_twig.php
@@ -3,34 +3,115 @@ private function transtext()
 {
 
 	//-------------------------------------------------------content\home\display.html
-	echo T_("Register");                                                              // Line 164
-	echo T_("Links");                                                                 // Line 227
-	echo T_("About Us");                                                              // Line 170
-	echo T_("Our answer sheets are absolutely correct, and we can measure the accuracy of answers");// Line 142
-	echo T_("Do you want to register an account?");                                   // Line 149
-	echo T_("Home");                                                                  // Line 161
-	echo T_("Articles");                                                              // Line 231
-	echo T_("News");                                                                  // Line 235
-	echo T_("Latest News");                                                           // Line 239
-	echo T_("Policy");                                                                // Line 243
-	echo T_("Proudly Made in IRAN");                                                  // Line 273
+	echo T_("Sarshomar");                                                             // Line 8 Seperate
+	echo T_("Our answer sheets are absolutely correct, and we can measure the accuracy of answers");// Line 150
 
-	//----------------------------------------------------content\home\layout-xhr.html
+	//----------------------------------------------------content\template\footer.html
+	echo T_("Home");                                                                  // Line 3
+	echo T_("Register");                                                              // Line 5
+	echo T_("Articles");                                                              // Line 29
+	echo T_("About Us");                                                              // Line 7
+	echo T_("Links");                                                                 // Line 4
+	echo T_("News");                                                                  // Line 30
+	echo T_("Latest News");                                                           // Line 31
+	echo T_("Policy");                                                                // Line 32
+	echo T_("Proudly Made in IRAN");                                                  // Line 52
+
+	//----------------------------------------------------content\template\header.html
+	echo T_("Guide");                                                                 // Line 5
+
+	//--------------------------------------------------content\template\poll.old.html
 	echo T_("View your current location and navigate to parent of it");               // Line 9
-
-	//---------------------------------------------------content\template\dafault.html
 	echo T_("Some right reserved!");                                                  // Line 29
-
-	//------------------------------------------------------content\template\poll.html
 	echo T_("No. of");                                                                // Line 45
 	echo T_("User Votes");                                                            // Line 45
 	echo T_("vote");                                                                  // Line 53
 	echo T_("Total");                                                                 // Line 72
 	echo T_("Bots");                                                                  // Line 77
 
+	//----------------------------------------------content\template\static_about.html
+	echo T_("About");                                                                 // Line 5
+
+	//------------------------------------------------content\template\static_add.html
+	echo T_("Gozinei");                                                               // Line 17
+	echo T_("descriptive");                                                           // Line 18
+	echo T_("Notification");                                                          // Line 22
+	echo T_("Upload");                                                                // Line 23
+	echo T_("Starred");                                                               // Line 24
+	echo T_("Shomarei");                                                              // Line 22
+	echo T_("Sort");                                                                  // Line 26
+
+	//--------------------------------------------content\template\static_contact.html
+	echo T_("Contact");                                                               // Line 5
+
+	//------------------------------------------------content\template\static_eco.html
+	echo T_("Eco");                                                                   // Line 5
+
+	//-------------------------------------------content\template\static_elements.html
+	echo T_("Elements");                                                              // Line 10
+	echo T_("Input");                                                                 // Line 11
+	echo T_("Basic");                                                                 // Line 12
+	echo T_("Basic with placeholder");                                                // Line 18
+	echo T_("Please enter text");                                                     // Line 39
+	echo T_("Basic with placeholder and icon");                                       // Line 23
+	echo T_("Primary with icon");                                                     // Line 29
+	echo T_("Primary with icon and placeholder");                                     // Line 36
+	echo T_("Success");                                                               // Line 43
+	echo T_("Why men marry women?");                                                  // Line 77
+	echo T_("Warn");                                                                  // Line 50
+	echo T_("Fail");                                                                  // Line 57
+	echo T_("Disabled");                                                              // Line 64
+	echo T_("Tags");                                                                  // Line 291
+	echo T_("Add");                                                                   // Line 296
+	echo T_("Slug");                                                                  // Line 100
+
+	//------------------------------------------------content\template\static_faq.html
+	echo T_("Sarshomar FAQ");                                                         // Line 5
+
+	//-------------------------------------------content\template\static_features.html
+	echo T_("Features");                                                              // Line 5
+
+	//--------------------------------------------content\template\static_privacy.html
+	echo T_("Privacy");                                                               // Line 5
+
+	//-------------------------------------------content\template\static_register.html
+	echo T_("Do you want to register an account?");                                   // Line 3
+
+	//----------------------------------------------content\template\static_terms.html
+	echo T_("Terms");                                                                 // Line 5
+
+	//-------------------------------------------content\template\static_visitors.html
+	echo T_("Visitors");                                                              // Line 5
+
+	//------------------------------------------------------content_u\add\display.html
+	echo T_("Multiple Choice");                                                       // Line 20
+	echo T_("Descriptive");                                                           // Line 21
+	echo T_("Numerical");                                                             // Line 25
+	echo T_("Poll type");                                                             // Line 36
+	echo T_("Title");                                                                 // Line 178
+	echo T_("Content");                                                               // Line 188
+	echo T_("Answer type");                                                           // Line 93
+	echo T_("True answer");                                                           // Line 107
+	echo T_("Point");                                                                 // Line 114
+	echo T_("Answer one");                                                            // Line 88
+	echo T_("Answer Two");                                                            // Line 119
+	echo T_("Language");                                                              // Line 183
+	echo T_("Publish date");                                                          // Line 193
+	echo T_("Can update");                                                            // Line 138
+	echo T_("Can remove");                                                            // Line 143
+	echo T_("Can stop");                                                              // Line 148
+	echo T_("Summary");                                                               // Line 155
+	echo T_("Edit");                                                                  // Line 205
+	echo T_("Write your tag and press enter to add new keyword to content");          // Line 290
+	echo T_("Enter Tags here");                                                       // Line 294
+	echo T_("Enter Tags");                                                            // Line 295
+
+	//-----------------------------------------------------content_u\home\display.html
+	echo T_("Username");                                                              // Line 28
+	echo T_("Description");                                                           // Line 29
+
 	//--------------------------------------------includes\languages\trans_static.html
 	echo T_("Saloos");                                                                // Line 7 Seperate
-	echo T_("Sarshomar");                                                             // Line 8 Seperate
 	echo T_("Sarshomar start jumping");                                               // Line 9 Seperate
 	echo T_("Call Sarshomar");                                                        // Line 10 Seperate
 	echo T_("Sarshomar is inteligent");                                               // Line 11 Seperate
