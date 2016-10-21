cf46dcfb49b2936911193cc67417a2c07801cbc8
Reza Mohiti <rm.biqarar@gmail.com>
2016-10-21 08:56:05 +0330

    profile page get age and range

--- content_u/me/model.php
+++ content_u/me/model.php
@@ -13,7 +13,9 @@ class model extends \mvc\model
 		$user_id = $this->login("id");
 		$me = \lib\db\profiles::get_profile_data($user_id);
 		$displayname = \lib\db\users::get_displayname($user_id);
+		$email = \lib\db\users::get_email($user_id);
 		$me['displayname'] = $displayname;
+		$me['email'] = $email;
 		return $me;
 	}
 
@@ -25,28 +27,49 @@ class model extends \mvc\model
 	{
 		$user_id = $this->login('id');
 
-		$displayname       = utility::post("displayname");
+		$displayname = utility::post("displayname");
 		$displayname = \lib\db\users::set_displayname($user_id, $displayname);
 
-		$support_filter = \lib\db\filters::support_filter();
+		$email = utility::post("email");
+		$email = \lib\db\users::set_email($user_id, $email);
 
-		$post = utility::post();
+		$age = self::get_age(utility::post("birthdate"));
+		$range = self::get_range($age);
+		$arg =
+		[
+			'name'            => utility::post("name"),
+			'lastname'        => utility::post("lastname"),
+			'gender'          => utility::post("gender"),
+			'marrital_status' => utility::post("marrital_status"),
+			'mobile'          => utility::post("mobile"),
+			'birthdate'       => utility::post("birthdate"),
+			'language'        => utility::post("language"),
+			'graduation'      => utility::post("graduation"),
+			'education_type'  => utility::post("education_type"),
+			'course'          => utility::post("course"),
+			'grade'           => utility::post("grade"),
+			'degree'          => utility::post("degree"),
+			'employment'      => utility::post("employment"),
+			'business'        => utility::post("business"),
+			'industry'        => utility::post("industry"),
+			'countrybirth'    => utility::post("countrybirth"),
+			'country'         => utility::post("country"),
+			'provincebirth'   => utility::post("provincebirth"),
+			'province'        => utility::post("province"),
+			'birthcity'       => utility::post("birthcity"),
+			'city'            => utility::post("city"),
+			'parental'        => utility::post("parental"),
+			'exercise'        => utility::post("exercise"),
+			'devices'         => utility::post("devices"),
+			'internetusage'   => utility::post("internetusage"),
+			'favorites'       => utility::post("favorites"),
+			'age'             => $age,
+			'range'			  => $range
+		];
+		// remove empty
+		$arg = array_filter($arg);
 
-		$args = [];
-		foreach ($post as $key => $value) {
-			if(\lib\db\filters::support_filter($key))
-			{
-				$args[$key] = $value;
-			}
-		}
-
-		if(isset($args['birthdate']))
-		{
-			$age = self::get_age($args['birthdate']);
-			$args['age'] = $age;
-		}
-
-		$profiles = \lib\db\profiles::set_profile_data($user_id, $args);
+		$profiles = \lib\db\profiles::set_profile_data($user_id, $arg);
 
 		if($profiles)
 		{
--- includes/mvc/model.php
+++ includes/mvc/model.php
@@ -9,19 +9,81 @@ class model extends \lib\mvc\model
 	 *
 	 * @param      <type>  $_brithday  The brithday
 	 */
-    public static function get_age($_brithday)
+    public function get_age($_brithday)
     {
-    	if(!\lib\utility\jdate::is_jalali($_brithday))
-    	{
-    		$brithday = \lib\utility\jdate::toGregorian($_brithday);
-    	}
-    	else
-    	{
-    		$brithday = explode("-", $_brithday);
-    	}
+        if($_brithday == null)
+        {
+            return null;
+        }
 
-		$age = date("Y") - $brithday[0];
+        $brith_year  = date("Y", strtotime($_brithday));
+        $brith_month = date("m", strtotime($_brithday));
+        $brith_day   = date("d", strtotime($_brithday));
+        // to convert the jalali date to gregorian date
+        if(intval($brith_year) > 1300 && intval($brith_year) < 1400)
+        {
+            list($brith_year, $brith_month, $brith_day) = \lib\utility\jdate::toGregorian($brith_year, $brith_month, $brith_day);
+            if($brith_month < 10)
+            {
+                $brith_month = "0". $brith_month;
+            }
+            if($brith_day < 10)
+            {
+                $brith_day = "0". $brith_day;
+            }
+        }
+        // get date diff
+        $date1 = new \DateTime($brith_year. $brith_month. $brith_day);
+        $date2 = new \DateTime("now");
+        $age   = $date1->diff($date2);
+        $age   = $age->y;
 		return $age;
+    }
+
+
+    /**
+     * Gets the range.
+     *
+     * @param      integer  $_age   The age
+     *
+     * @return     string   The range.
+     */
+    public function get_range($_age)
+    {
+        $range = null;
+
+        $_age = intval($_age);
+
+        switch ($_age) {
+            case $_age < 18 :
+                $range = "less than 18";
+                break;
+
+            case $_age >= 18 && $_age <= 24 :
+                $range = "between 18 and 24";
+                break;
+
+            case $_age >= 25 && $_age <= 34 :
+                $range = "between 25 and 34";
+                break;
+
+            case $_age >= 35 && $_age < 44 :
+                $range = "between 35 and 44";
+                break;
+
+            case $_age >= 45 && $_age <= 54 :
+                $range = "between 45 and 54";
+                break;
+
+            case $_age >= 55 && $_age <= 64 :
+                $range = "between 55 and 64";
+                break;
+
+            case $_age >= 65:
+                $range = "more than 65";
+                break;
+        }
+        return $range;
     }
 }
 ?>
