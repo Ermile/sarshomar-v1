a34d1c5ddde41aae0814002fc1b7237c192357f5
Reza Mohiti <rm.biqarar@gmail.com>
2016-10-21 09:29:23 +0330

    redirect fa/@ to @?lang=fa

--- includes/ilib/main.php
+++ includes/ilib/main.php
@@ -12,6 +12,13 @@ class main extends \lib\main
 			$redirect->redirect();
 		}
 
+		if(preg_match("/^\/(\w{2})\/\@/", $_SERVER['REQUEST_URI'], $lang))
+		{
+			$redirect = new \lib\redirector();
+			$redirect->url = "/@?lang=$lang[1]";
+			$redirect->redirect();
+		}
+
 		if(\lib\router::get_class() == '@')
 		{
 			$url = preg_replace("/^\/@/", '', $_SERVER['REQUEST_URI']);
