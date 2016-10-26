20a149d31f43efb2ed1fc33685ea8b9d6033e074
baravak <itb.baravak@gmail.com>
2016-10-26 15:38:04 +0330

    db tg_session add

---
+++ includes/lib/db/tg_session.php
@@ -0,0 +1,94 @@
+<?php
+namespace lib\db;
+
+class tg_session
+{
+	public static $start = false;
+	public static $data, $user_id;
+
+	public static function start($_user_id = null)
+	{
+		$user_id = $_user_id;
+		if(!$user_id)
+		{
+			$user_id = self::$user_id;
+		}
+		$get_sesstion = "SELECT * FROM options
+		WHERE options.option_cat = 'telegram' AND
+		options.user_id = $user_id AND
+		options.option_key = 'session' AND
+		options.option_value = '$user_id'
+		LIMIT 1";
+		$result = \lib\db::get($get_sesstion);
+		if($result)
+		{
+			self::$start = true;
+		}
+		$result = \lib\utility\filter::meta_decode($result, null, ['return_object' => true]);
+		if(array_key_exists('option_meta', $result))
+		{
+			self::$data = $result;
+		}
+		else
+		{
+			self::$data = (object) array();
+		}
+		return self::$data;
+	}
+
+	public static function set($_keys, $_value)
+	{
+		if(!self::$start)
+		{
+			self::start();
+		}
+		$args = func_get_args();
+		$arg_value = end($args);
+		$keys = array_splice($args, 0 , count($args) -1);
+		$object = self::$data;
+		$prefix_keys = array_splice($keys, 0, count($keys) -1);
+		$arg_key = end($keys);
+		foreach ($prefix_keys as $key => $value) {
+			if(!isset($object->$value))
+			{
+				$object->$value = (object) array();
+			}
+			$object = $object->$value;
+		}
+		$object->$arg_key = $arg_value;
+	}
+
+	public static function get(...$_keys)
+	{
+		$object = self::$data;
+		foreach ($_keys as $key => $value) {
+			if(!isset($object->$value))
+			{
+				return null;
+			}
+			$object = $object->$value;
+		}
+		return $object;
+	}
+
+	public static function save($_user_id = null)
+	{
+		$user_id = $_user_id;
+		if(!$user_id)
+		{
+			$user_id = self::$user_id;
+		}
+		$meta = json_encode(self::$data);
+		$query = "INSERT INTO options SET
+		options.user_id = $user_id,
+		options.option_cat = 'telegram',
+		options.option_key = 'session',
+		options.option_value = '$user_id',
+		options.option_meta = '$meta'
+		ON DUPLICATE KEY UPDATE
+		options.option_meta = '$meta'
+		";
+		return \lib\db::query($query);
+	}
+}
+?>
