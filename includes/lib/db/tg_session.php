8659c4bda996e75d1bdd23fe874d5d79faded88e
baravak <itb.baravak@gmail.com>
2016-10-29 10:09:27 +0330

    json newline bugfix

--- includes/lib/db/tg_session.php
+++ includes/lib/db/tg_session.php
@@ -4,10 +4,14 @@ namespace lib\db;
 class tg_session
 {
 	public static $start = false;
-	public static $data, $user_id;
+	public static $data, $user_id, $data_back;
 
 	public static function start($_user_id = null)
 	{
+		if(self::$start)
+		{
+			return self::$data;
+		}
 		$user_id = $_user_id;
 		if(!$user_id)
 		{
@@ -19,41 +23,42 @@ class tg_session
 		options.option_key = 'session' AND
 		options.option_value = '$user_id'
 		LIMIT 1";
-		$result = \lib\db::get($get_sesstion);
-		if($result)
+		$original_result = \lib\db::get($get_sesstion);
+		if($original_result)
 		{
 			self::$start = true;
 		}
-		$result = \lib\utility\filter::meta_decode($result, null, ['return_object' => true]);
-		if(array_key_exists(0, $result))
+		if(array_key_exists(0, $original_result) && array_key_exists('option_meta', $original_result[0]))
 		{
-			$result = $result[0];
-		}
-		if(array_key_exists('option_meta', $result))
-		{
-			self::$data = $result;
+
+			$json_result = \lib\utility\filter::meta_decode($original_result, null, ['return_object' => true]);
+			self::$data = $json_result[0]['option_meta'];
+
+			$json_result = \lib\utility\filter::meta_decode($original_result, null, ['return_object' => true]);
+			self::$data_back = $json_result[0]['option_meta'];
 		}
 		else
 		{
 			self::$data = (object) array();
+			self::$data_back = (object) array();
 		}
+
 		return self::$data;
 	}
 
 	public static function set($_keys, $_value)
 	{
-		if(!self::$start)
-		{
-			self::start();
-		}
+		self::$start;
 		$args = func_get_args();
+		$object = self::$data;
+
+		if($args && is_array($args[0]))
+		{
+			$object = $args[1];
+			$args = $args[0];
+		}
 		$arg_value = end($args);
 		$keys = array_splice($args, 0 , count($args) -1);
-		$object = self::$data;
-		if(count($keys) === 1)
-		{
-			return $object = $_value;
-		}
 
 		$prefix_keys = array_splice($keys, 0, count($keys) -1);
 		$arg_key = end($keys);
@@ -69,8 +74,18 @@ class tg_session
 
 	public static function get(...$_keys)
 	{
+		self::$start;
+
+		$args = func_get_args();
 		$object = self::$data;
-		foreach ($_keys as $key => $value) {
+
+		if($args && is_array($args[0]))
+		{
+			$object = $args[1];
+			$args = $args[0];
+		}
+
+		foreach ($args as $key => $value) {
 			if(!isset($object->$value))
 			{
 				return null;
@@ -82,32 +97,80 @@ class tg_session
 
 	public static function push($_keys, $_value)
 	{
+		self::$start;
+
 		$args = func_get_args();
+		$object = self::$data;
+
+		if($args && is_array($args[0]))
+		{
+			$object = $args[1];
+			$args = $args[0];
+		}
+
 		$arg_value = end($args);
 		$keys = array_splice($args, 0 , count($args) -1);
 		$is_exists = self::get(...$keys);
 		$x = $keys;
 		if(!$is_exists)
 		{
-			array_push($keys, [$_value]);
+			array_push($keys, [$arg_value]);
 			self::set(...$keys);
 		}
 		else
 		{
-			array_push($is_exists, $_value);
+			if(!is_array($is_exists))
+			{
+				$is_exists = [$is_exists];
+			}
+			array_push($is_exists, $arg_value);
 			array_push($keys, $is_exists);
 			self::set(...$keys);
 		}
 	}
 
+	public static function remove(...$_keys)
+	{
+		self::$start;
+
+		$args = func_get_args();
+		$object = self::$data;
+
+		if($args && is_array($args[0]))
+		{
+			$object = $args[1];
+			$args = $args[0];
+		}
+
+		$end = count($args) - 1;
+		foreach ($args as $key => $value) {
+			if(!isset($object->$value))
+			{
+				return false;
+			}
+
+			if($key === $end)
+			{
+				unset($object->$value);
+				return true;
+			}
+			else
+			{
+				$object = $object->$value;
+			}
+		}
+	}
+
 	public static function save($_user_id = null)
 	{
+		self::$start;
+
 		$user_id = $_user_id;
 		if(!$user_id)
 		{
 			$user_id = self::$user_id;
 		}
-		$meta = json_encode(self::$data);
+		$meta = json_encode(self::$data, JSON_UNESCAPED_UNICODE);
 		$query = "INSERT INTO options SET
 		options.user_id = $user_id,
 		options.option_cat = 'telegram',
@@ -115,9 +178,17 @@ class tg_session
 		options.option_value = '$user_id',
 		options.option_meta = '$meta'
 		ON DUPLICATE KEY UPDATE
-		options.option_meta = '$meta'
-		";
+		options.option_meta = '$meta'";
 		return \lib\db::query($query);
+	}
+
+	public static function __callStatic($_name, $_args)
+	{
+		if(preg_match("/^(.*)_back$/", $_name, $name))
+		{
+			$method_name = $name[1];
+			return self::$method_name($_args, self::$data_back);
+		}
 	}
 }
 ?>
