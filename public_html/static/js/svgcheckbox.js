30b1ed8d74dd81d33694030b9f9a325fa62afd0a
Javad <J.Evazzadeh@live.com>
2016-11-06 20:51:44 +0330

    completely change style of check boxes

--- public_html/static/js/svgcheckbox.js
+++ public_html/static/js/svgcheckbox.js
@@ -1,16 +1,15 @@
 if( document.createElement('svg').getAttributeNS )
 {
-
-
-	var checkbxsCross = Array.prototype.slice.call( document.querySelectorAll( '.ac-cross input[type="checkbox"]' ) ),
-		radiobxsFill = Array.prototype.slice.call( document.querySelectorAll( '.ac-fill input[type="radio"]' ) ),
+	var checkbxsCross     = Array.prototype.slice.call( document.querySelectorAll( '.ac-cross input[type="checkbox"]' ) ),
 		checkbxsCheckmark = Array.prototype.slice.call( document.querySelectorAll( '.ac-checkmark input[type="checkbox"]' ) ),
-		radiobxsCircle = Array.prototype.slice.call( document.querySelectorAll( '.ac-circle input[type="radio"]' ) ),
-		checkbxsBoxfill = Array.prototype.slice.call( document.querySelectorAll( '.ac-boxfill input[type="checkbox"]' ) ),
-		radiobxsSwirl = Array.prototype.slice.call( document.querySelectorAll( '.ac-swirl input[type="radio"]' ) ),
-		checkbxsDiagonal = Array.prototype.slice.call( document.querySelectorAll( '.ac-diagonal input[type="checkbox"]' ) ),
-		checkbxsList = Array.prototype.slice.call( document.querySelectorAll( '.ac-list input[type="checkbox"]' ) ),
-		pathDefs = {
+		checkbxsBoxfill   = Array.prototype.slice.call( document.querySelectorAll( '.ac-boxfill input[type="checkbox"]' ) ),
+		checkbxsDiagonal  = Array.prototype.slice.call( document.querySelectorAll( '.ac-diagonal input[type="checkbox"]' ) ),
+		checkbxsList      = Array.prototype.slice.call( document.querySelectorAll( '.ac-list input[type="checkbox"]' ) ),
+		radiobxsFill      = Array.prototype.slice.call( document.querySelectorAll( '.ac-fill input[type="radio"]' ) ),
+		radiobxsCircle    = Array.prototype.slice.call( document.querySelectorAll( '.ac-circle input[type="radio"]' ) ),
+		radiobxsSwirl     = Array.prototype.slice.call( document.querySelectorAll( '.ac-swirl input[type="radio"]' ) ),
+		pathDefs =
+		{
 			cross : ['M 10 10 L 90 90','M 90 10 L 10 90'],
 			fill : ['M15.833,24.334c2.179-0.443,4.766-3.995,6.545-5.359 c1.76-1.35,4.144-3.732,6.256-4.339c-3.983,3.844-6.504,9.556-10.047,13.827c-2.325,2.802-5.387,6.153-6.068,9.866 c2.081-0.474,4.484-2.502,6.425-3.488c5.708-2.897,11.316-6.804,16.608-10.418c4.812-3.287,11.13-7.53,13.935-12.905 c-0.759,3.059-3.364,6.421-4.943,9.203c-2.728,4.806-6.064,8.417-9.781,12.446c-6.895,7.477-15.107,14.109-20.779,22.608 c3.515-0.784,7.103-2.996,10.263-4.628c6.455-3.335,12.235-8.381,17.684-13.15c5.495-4.81,10.848-9.68,15.866-14.988 c1.905-2.016,4.178-4.42,5.556-6.838c0.051,1.256-0.604,2.542-1.03,3.672c-1.424,3.767-3.011,7.432-4.723,11.076 c-2.772,5.904-6.312,11.342-9.921,16.763c-3.167,4.757-7.082,8.94-10.854,13.205c-2.456,2.777-4.876,5.977-7.627,8.448 c9.341-7.52,18.965-14.629,27.924-22.656c4.995-4.474,9.557-9.075,13.586-14.446c1.443-1.924,2.427-4.939,3.74-6.56 c-0.446,3.322-2.183,6.878-3.312,10.032c-2.261,6.309-5.352,12.53-8.418,18.482c-3.46,6.719-8.134,12.698-11.954,19.203 c-0.725,1.234-1.833,2.451-2.265,3.77c2.347-0.48,4.812-3.199,7.028-4.286c4.144-2.033,7.787-4.938,11.184-8.072 c3.142-2.9,5.344-6.758,7.925-10.141c1.483-1.944,3.306-4.056,4.341-6.283c0.041,1.102-0.507,2.345-0.876,3.388 c-1.456,4.114-3.369,8.184-5.059,12.212c-1.503,3.583-3.421,7.001-5.277,10.411c-0.967,1.775-2.471,3.528-3.287,5.298 c2.49-1.163,5.229-3.906,7.212-5.828c2.094-2.028,5.027-4.716,6.33-7.335c-0.256,1.47-2.07,3.577-3.02,4.809'],
 			checkmark : ['M16.667,62.167c3.109,5.55,7.217,10.591,10.926,15.75 c2.614,3.636,5.149,7.519,8.161,10.853c-0.046-0.051,1.959,2.414,2.692,2.343c0.895-0.088,6.958-8.511,6.014-7.3 c5.997-7.695,11.68-15.463,16.931-23.696c6.393-10.025,12.235-20.373,18.104-30.707C82.004,24.988,84.802,20.601,87,16'],
@@ -20,7 +19,8 @@ if( document.createElement('svg').getAttributeNS )
 			diagonal : ['M16.053,91.059c0.435,0,0.739-0.256,0.914-0.768 c3.101-2.85,5.914-6.734,8.655-9.865C41.371,62.438,56.817,44.11,70.826,24.721c3.729-5.16,6.914-10.603,10.475-15.835 c0.389-0.572,0.785-1.131,1.377-1.521'],
 			list : ['M1.986,8.91c41.704,4.081,83.952,5.822,125.737,2.867 c17.086-1.208,34.157-0.601,51.257-0.778c21.354-0.223,42.706-1.024,64.056-1.33c18.188-0.261,36.436,0.571,54.609,0.571','M3.954,25.923c9.888,0.045,19.725-0.905,29.602-1.432 c16.87-0.897,33.825-0.171,50.658-2.273c14.924-1.866,29.906-1.407,44.874-1.936c19.9-0.705,39.692-0.887,59.586,0.45 c35.896,2.407,71.665-1.062,107.539-1.188']
 		},
-		animDefs = {
+		animDefs =
+		{
 			cross : { speed : .2, easing : 'ease-in-out' },
 			fill : { speed : .8, easing : 'ease-in-out' },
 			checkmark : { speed : .2, easing : 'ease-in-out' },
@@ -31,37 +31,46 @@ if( document.createElement('svg').getAttributeNS )
 			list : { speed : .3, easing : 'ease-in-out' }
 		};
 
-	function createSVGEl( def ) {
+	function createSVGEl( def )
+	{
 		var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-		if( def ) {
+		if( def )
+		{
 			svg.setAttributeNS( null, 'viewBox', def.viewBox );
 			svg.setAttributeNS( null, 'preserveAspectRatio', def.preserveAspectRatio );
 		}
-		else {
+		else
+		{
 			svg.setAttributeNS( null, 'viewBox', '0 0 100 100' );
 		}
 		svg.setAttribute( 'xmlns', 'http://www.w3.org/2000/svg' );
 		return svg;
 	}
 
-	function controlCheckbox( el, type, svgDef ) {
+	function controlCheckbox( el, type, svgDef )
+	{
 		var svg = createSVGEl( svgDef );
 		el.parentNode.appendChild( svg );
 
-		el.addEventListener( 'change', function() {
-			if( el.checked ) {
+		el.addEventListener( 'change', function()
+		{
+			if( el.checked )
+			{
 				draw( el, type );
 			}
-			else {
+			else
+			{
 				reset( el );
 			}
 		} );
 	}
 
-	function controlRadiobox( el, type ) {
+	function controlRadiobox( el, type )
+	{
 		var svg = createSVGEl();
 		el.parentNode.appendChild( svg );
-		el.addEventListener( 'change', function() {
+		el.addEventListener( 'change', function()
+		{
 			resetRadio( el );
 			draw( el, type );
 		} );
@@ -76,12 +85,14 @@ if( document.createElement('svg').getAttributeNS )
 	checkbxsDiagonal.forEach( function( el, i ) { controlCheckbox( el, 'diagonal' ); } );
 	checkbxsList.forEach( function( el ) { controlCheckbox( el, 'list', { viewBox : '0 0 300 100', preserveAspectRatio : 'none' } ); } );
 
-	function draw( el, type ) {
+	function draw( el, type )
+	{
 		var paths = [], pathDef,
 			animDef,
 			svg = el.parentNode.querySelector( 'svg' );
 
-		switch( type ) {
+		switch( type )
+		{
 			case 'cross': pathDef = pathDefs.cross; animDef = animDefs.cross; break;
 			case 'fill': pathDef = pathDefs.fill; animDef = animDefs.fill; break;
 			case 'checkmark': pathDef = pathDefs.checkmark; animDef = animDefs.checkmark; break;
@@ -94,11 +105,13 @@ if( document.createElement('svg').getAttributeNS )
 
 		paths.push( document.createElementNS('http://www.w3.org/2000/svg', 'path' ) );
 
-		if( type === 'cross' || type === 'list' ) {
+		if( type === 'cross' || type === 'list' )
+		{
 			paths.push( document.createElementNS('http://www.w3.org/2000/svg', 'path' ) );
 		}
 
-		for( var i = 0, len = paths.length; i < len; ++i ) {
+		for( var i = 0, len = paths.length; i < len; ++i )
+		{
 			var path = paths[i];
 			svg.appendChild( path );
 
@@ -123,17 +136,19 @@ if( document.createElement('svg').getAttributeNS )
 		}
 	}
 
-	function reset( el ) {
+	function reset( el )
+	{
 		Array.prototype.slice.call( el.parentNode.querySelectorAll( 'svg > path' ) ).forEach( function( el ) { el.parentNode.removeChild( el ); } );
 	}
 
-	function resetRadio( el ) {
+	function resetRadio( el )
+	{
 		Array.prototype.slice.call( document.querySelectorAll( 'input[type="radio"][name="' + el.getAttribute( 'name' ) + '"]' ) ).forEach( function( el ) {
 			var path = el.parentNode.querySelector( 'svg > path' );
-			if( path ) {
+			if( path )
+			{
 				path.parentNode.removeChild( path );
 			}
 		} );
 	}
-
 }
