f054a471136e755b4ee026c1b59a7e7d949e658e
Reza Mohiti <rm.biqarar@gmail.com>
2016-11-08 15:18:59 +0330

    add permission

--- content_u/manifest.php
+++ content_u/manifest.php
@@ -1,9 +1,32 @@
 <?php
 $modules = array();
-$modules['sarshomar'] = array(
-	'desc' 			=> T_('can add, edit poll or survey of sarshomar'),
+/**
+ * poll to complete profile
+ */
+$modules['complete_profile'] = array(
+	'desc' 			=> T_("can add the poll to complete the profile"),
 	'icon'			=> 'file-text-o',
-	'permissions'	=> ['view', 'add', 'edit', 'delete', 'admin'],
+	'permissions'	=> ['admin'],
 	);
+
+/**
+ *	sarshomart cat
+ *	set the cat of poll on sarshomar
+ */
+$modules['sarshomar_poll'] = array(
+	'desc' 			=> T_("can add the poll by sarshomar cat"),
+	'icon'			=> 'file-text-o',
+	'permissions'	=> ['add', 'edit', 'view', 'update', 'admin'],
+	);
+
+/**
+ * show hidden result checkbox
+ */
+$modules['hidden_result'] = array(
+	'desc' 			=> T_("can hidden result poll"),
+	'icon'			=> 'file-text-o',
+	'permissions'	=> ['admin'],
+	);
+
 return ["modules" => $modules];
 ?>
