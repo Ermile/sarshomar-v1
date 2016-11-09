ca4024c78b7cb1198be2733b51b34c8793417b5a
Javad <J.Evazzadeh@live.com>
2016-11-09 09:34:47 +0330

    clean js file

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -1,6 +1,8 @@
 // contact form
-route('contact', function(){
-	$('form').on('ajaxify:success',function(data, debug){
+route('contact', function()
+{
+	$('form').on('ajaxify:success',function(data, debug)
+	{
 		if(debug.status)
 		{
 			$('input').val('');
@@ -9,15 +11,18 @@ route('contact', function(){
 	});
 });
 
+
 // Add
 route('@/add', function()
 {
-	$(this).on('click','button', function(){
+	$(this).on('click','button', function()
+	{
 		$('#submit-form').attr("value", $(this).attr("send-name"));
 		$('#submit-form').attr("name", $(this).attr("send-name"));
 	});
 	// run on input change
-	$(this).on('input', '.element.small .input[type="text"]', function(event) {
+	$(this).on('input', '.element.small .input[type="text"]', function(event)
+	{
 		var number_of_empty_inputs = 0;
 
 		// check if current element has not value and we have no empty inputs
@@ -43,7 +48,9 @@ route('@/add', function()
 
 		// if we had empty inputs do this
 		else
-		{}
+		{
+
+		}
 	});
 
 	$(this).on('mouseenter', '.element.small', function()
@@ -79,54 +86,65 @@ route('@/add', function()
 
 
 // Me | Profile
-route('@/me', function () {
-		// btns is generated in display in order to true translation
-		$.each($('.element.raw'), function (key, value) {
-				if ($(this).children('.input').val()) {
-						$(this).dblclick(function () {
-								if (!$('.main').hasClass('editing')) {
-										$('.main').addClass('editing');
-										$(this).removeClass('raw').append(btns).children('.input').removeAttr('disabled').focus();
-								}
-						});
+route('@/me', function ()
+{
+	// btns is generated in display in order to true translation
+	$.each($('.element.raw'), function (key, value)
+	{
+		if ($(this).children('.input').val())
+		{
+			$(this).dblclick(function ()
+			{
+				if (!$('.main').hasClass('editing'))
+				{
+					$('.main').addClass('editing');
+					$(this).removeClass('raw').append(btns).children('.input').removeAttr('disabled').focus();
 				}
-				else {
-						$(this).click(function () {
-								if (!$('.main').hasClass('editing')) {
-										$('.main').addClass('editing');
-										$(this).removeClass('raw').append(btns).children('.input').removeAttr('disabled').focus();
-								}
-						});
+			});
+		}
+		else
+		{
+			$(this).click(function ()
+			{
+				if (!$('.main').hasClass('editing'))
+				{
+					$('.main').addClass('editing');
+					$(this).removeClass('raw').append(btns).children('.input').removeAttr('disabled').focus();
 				}
-		});
+			});
+		}
+	});
 
-		$(this).on('blur', '.element .input', function (event) {
-				$('.main').removeClass('editing');
-		});
+	$(this).on('blur', '.element .input', function (event)
+	{
+		$('.main').removeClass('editing');
+	});
 
-		$(this).on('click', '.btn.save button', function (event) {
-				var val = $(this).parents('.element').children('.input').val();
-				var name = $(this).parents('.element').children('.input').attr("name");
-				$(this).ajaxify(
-					{
-						ajax:
-						{
-							data:
-							{
-								'name': name,
-								'value': val
-							},
-							abort: true,
-							success: function(e, data, x)
-							{
+	$(this).on('click', '.btn.save button', function (event)
+	{
+		var val = $(this).parents('.element').children('.input').val();
+		var name = $(this).parents('.element').children('.input').attr("name");
+		$(this).ajaxify(
+		{
+			ajax:
+			{
+				data:
+				{
+					'name': name,
+					'value': val
+				},
+				abort: true,
+				success: function(e, data, x)
+				{
 
-							},
-							url: '@/me',
-							method: 'post'
-						}
-					});
+				},
+				url: '@/me',
+				method: 'post'
+			}
 		});
+	});
 });
+
 
 // pollsearch | Knowledge
 // route(/^\$(.*)$/, function (_doc)
@@ -149,16 +167,19 @@ route('*', function (_doc)
 				Navigate({ url: '$' + val });
 			}
 				change -= 1;
-		}, 500);
+		}, 250);
 	});
 	$('.pollsearch', _doc).focus().val($('.pollsearch').val());
 });
 
 
 // Me | Profile
-route('*', function () {
-	$.each($('input.autocomplete'),function(){
-		$(this).keyup(function(e){
+route('*', function ()
+{
+	$.each($('input.autocomplete'),function()
+	{
+		$(this).keyup(function(e)
+		{
 			name = $(this).attr('name');
 			val = $(this).val();
 			$(this).ajaxify(
@@ -191,124 +212,136 @@ route('*', function () {
 
 
 
-route('*', function () {
+route('*', function ()
+{
 	$('#tag-add').keypress(function (e)
 	{
-			// if Enter pressed disallow it and run add func
-			if (e.which == 13)
-			{
-					addTag();
-					return false;
-			}
+		// if Enter pressed disallow it and run add func
+		if (e.which == 13)
+		{
+			addTag();
+			return false;
+		}
 	});
 
 	$('#features .wrapper .features li').on("mouseover", function (ev) { addClass( ev, this, 'in' ); });
 	$('#features .wrapper .features li').on("mouseout", function (ev) { addClass( ev, this, 'out' );});
 
 	$(document).on('click', '#tag-add-btn', function () { addTag(); });
-	$(document).on('click', '#tag-list span i', function () {
-			var span = $(this).parent();
-			$('#sp-tags').val($('#sp-tags').val().replace(span.text() + ', ', ''));
-			span.remove();
+	$(document).on('click', '#tag-list span i', function ()
+	{
+		var span = $(this).parent();
+		$('#sp-tags').val($('#sp-tags').val().replace(span.text() + ', ', ''));
+		span.remove();
 	});
 
 
-		var tagDefault = $('#sp-tags').val();
-		$('#tag-list').text('');
-		if (tagDefault) {
-				$.each(tagDefault.split(', '), function (t, item) {
-						if (item.trim())
-								$('#tag-list').append("<span><i class='fa fa-times'></i>" + item + "</span>");
-				});
+	var tagDefault = $('#sp-tags').val();
+	$('#tag-list').text('');
+	if (tagDefault)
+	{
+		$.each(tagDefault.split(', '), function (t, item)
+		{
+			if (item.trim())
+				$('#tag-list').append("<span><i class='fa fa-times'></i>" + item + "</span>");
+		});
+	}
+
+	// add tab support to cp
+	$('.tabs li').click(function()
+	{
+		var _this     = $(this);
+		var tabNum    = _this.attr('data-tab');
+		var tabsItems = _this.parent().children('li');
+		var tabGroup  = _this.parent().attr('data-group');
+		var tabSelected;
+		var tabItems
+		// if use group find it else use default tab value
+		if(tabGroup)
+		{
+			tabItems  = $('.tab[data-group="'+ tabGroup +'"]');
+		}
+		else
+		{
+			tabItems  = $('.tab');
 		}
 
-		// add tab support to cp
-		$('.tabs li').click(function()
+		// remove active class from all items and select clicked item
+		tabsItems.removeClass('active');
+		_this.addClass('active');
+
+		if(tabNum)
 		{
-			var _this     = $(this);
-			var tabNum    = _this.attr('data-tab');
-			var tabsItems = _this.parent().children('li');
-			var tabGroup  = _this.parent().attr('data-group');
-			var tabSelected;
-			var tabItems
-			// if use group find it else use default tab value
-			if(tabGroup)
-			{
-				tabItems  = $('.tab[data-group="'+ tabGroup +'"]');
-			}
-			else
-			{
-				tabItems  = $('.tab');
-			}
-
-			// remove active class from all items and select clicked item
-			tabsItems.removeClass('active');
-			_this.addClass('active');
-
-			if(tabNum)
-			{
-				tabSelected = tabItems.children("#tab-"+tabNum);
-				// $('[id^=tab-]').not(tabSelected).css('display', "none");
-				tabItems.children('[id^=tab-]').not(tabSelected).css('display', "none");
-			}
-			else
-			{
-				tabNum = _this.index()+1;
-				tabSelected = tabItems.children("li:nth-child("+tabNum+")");
-				tabItems.children('li').not(tabSelected).css('display', "none");
-			}
-			$(tabSelected).fadeIn(300);
-		})
-		// run click for first time and show content of active tab
-		$(".tabs").each(function()
+			tabSelected = tabItems.children("#tab-"+tabNum);
+			// $('[id^=tab-]').not(tabSelected).css('display', "none");
+			tabItems.children('[id^=tab-]').not(tabSelected).css('display', "none");
+		}
+		else
 		{
-			// if select one element as active select content of it
-			if($(this).children('li.active').length == 1)
-			{
-				$(this).children('li.active').trigger("click");
-				$('input[name="poll_type"], input[name="filter_type"]').val( $(this).children('li.active').data('tab') );
-			}
-			// else select first child
-			else
-			{
-				$(this).children('li:first-child').trigger("click");
-				$('input[name="poll_type"], input[name="filter_type"]').val($(this).children('li:first-child').data('tab'));
-			}
-		});
+			tabNum = _this.index()+1;
+			tabSelected = tabItems.children("li:nth-child("+tabNum+")");
+			tabItems.children('li').not(tabSelected).css('display', "none");
+		}
+		$(tabSelected).fadeIn(300);
+	})
+	// run click for first time and show content of active tab
+	$(".tabs").each(function()
+	{
+		// if select one element as active select content of it
+		if($(this).children('li.active').length == 1)
+		{
+			$(this).children('li.active').trigger("click");
+			$('input[name="poll_type"], input[name="filter_type"]').val( $(this).children('li.active').data('tab') );
+		}
+		// else select first child
+		else
+		{
+			$(this).children('li:first-child').trigger("click");
+			$('input[name="poll_type"], input[name="filter_type"]').val($(this).children('li:first-child').data('tab'));
+		}
+	});
 
-		// change poll_type on tabs click
-		$('.tabs li').click(function(){
-			$('input[name="poll_type"], input[name="filter_type"]').val( $(this).data('tab') );
-		});
+	// change poll_type on tabs click
+	$('.tabs li').click(function(){
+		$('input[name="poll_type"], input[name="filter_type"]').val( $(this).data('tab') );
+	});
 
 });
 
+
 // -------------------------------------------------- Add Tag
-function addTag() {
-		var tag = $('#tag-add');
-		var newTag = tag.val().trim();
-		if (newTag) {
-				var exist = false;
-				$.each($('#sp-tags').val().split(', '), function (t, item) {
-						if (item == newTag) { exist = t + 1; }
-				});
-				if (exist) {
-						existEl = $("#tag-list span:nth-child(" + exist + ")");
-						bg = existEl.css('background-color');
-						existEl.css('background-color', '#ddd');
-						setTimeout(function () { existEl.css("background-color", bg) }, 500);
-				} else {
-						$('#tag-list').append("<span><i class='fa fa-times'></i>" + newTag + "</span>");
-						$('#sp-tags').val($('#sp-tags').val() + newTag + ', ');
-				}
+function addTag()
+{
+	var tag = $('#tag-add');
+	var newTag = tag.val().trim();
+	if (newTag)
+	{
+		var exist = false;
+		$.each($('#sp-tags').val().split(', '), function (t, item)
+		{
+			if (item == newTag) { exist = t + 1; }
+		});
+		if (exist)
+		{
+			existEl = $("#tag-list span:nth-child(" + exist + ")");
+			bg = existEl.css('background-color');
+			existEl.css('background-color', '#ddd');
+			setTimeout(function () { existEl.css("background-color", bg) }, 500);
 		}
-		tag.val('');
+		else
+		{
+			$('#tag-list').append("<span><i class='fa fa-times'></i>" + newTag + "</span>");
+			$('#sp-tags').val($('#sp-tags').val() + newTag + ', ');
+		}
+	}
+	tag.val('');
 }
 
 
 // --------------------------------- Sliders ---------------------------------
 
-function pauseEvent(e){
+function pauseEvent(e)
+{
 	if (e.stopPropagation) e.stopPropagation();
 	if (e.preventDefault) e.preventDefault();
 	e.cancelBubble = true;
@@ -316,7 +349,8 @@ function pauseEvent(e){
 	return false;
 }
 
-$(function(){
+$(function()
+{
 	$('.input-slider').each(function(id, el){
 		$(el).append($('<div style="width: 10px;height: 25px;background: #666;position: absolute;top: 5px;left: 100px;cursor:pointer"></div>'));
 
@@ -392,30 +426,33 @@ $(function(){
 
 
 // -------------------------------------------------- Features
-var getDirection = function (ev, obj) {
-		var w = obj.offsetWidth,
-				h = obj.offsetHeight,
-				x = (ev.pageX - obj.offsetLeft - (w / 2) * (w > h ? (h / w) : 1)),
-				y = (ev.pageY - obj.offsetTop - (h / 2) * (h > w ? (w / h) : 1)),
-				d = Math.round( Math.atan2(y, x) / 1.57079633 + 5 ) % 4;
+var getDirection = function (ev, obj)
+{
+	var w = obj.offsetWidth,
+		h = obj.offsetHeight,
+		x = (ev.pageX - obj.offsetLeft - (w / 2) * (w > h ? (h / w) : 1)),
+		y = (ev.pageY - obj.offsetTop - (h / 2) * (h > w ? (w / h) : 1)),
+		d = Math.round( Math.atan2(y, x) / 1.57079633 + 5 ) % 4;
 
-		return d;
+	return d;
 };
 
-var addClass = function ( ev, obj, state ) {
-		var direction = getDirection( ev, obj ),
-				class_suffix = "";
+var addClass = function ( ev, obj, state )
+{
+	var direction = getDirection( ev, obj ),
+		class_suffix = "";
 
-		obj.className = "";
+	obj.className = "";
 
-		switch ( direction ) {
-				case 0 : class_suffix = '-top';    break;
-				case 1 : class_suffix = '-right';  break;
-				case 2 : class_suffix = '-bottom'; break;
-				case 3 : class_suffix = '-left';   break;
-		}
+	switch ( direction )
+	{
+			case 0 : class_suffix = '-top';    break;
+			case 1 : class_suffix = '-right';  break;
+			case 2 : class_suffix = '-bottom'; break;
+			case 3 : class_suffix = '-left';   break;
+	}
 
-		obj.classList.add( state + class_suffix );
+	obj.classList.add( state + class_suffix );
 };
 
 
