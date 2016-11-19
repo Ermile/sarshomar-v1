602502a8e11c05d2f59d20730b35b29d4db29a01
Reza Mohiti <rm.biqarar@gmail.com>
2016-11-19 13:42:17 +0330

    show comment_count

--- includes/lib/db/polls.php
+++ includes/lib/db/polls.php
@@ -11,26 +11,26 @@ class polls
 
 	public static $fields =
 	"
-			posts.id					as 'id',
-			posts.post_language 		as 'language',
-			posts.post_title 			as 'title',
-			posts.post_slug 			as 'slug',
-			posts.post_url 				as 'url',
-			posts.post_content 			as 'content',
-			posts.post_type 			as 'type',
-			posts.post_comment 			as 'comment',
-			posts.post_meta 			as 'meta',
-			posts.post_count 			as 'count',
-			posts.post_order 			as 'order',
-			posts.post_status 			as 'status',
-			posts.post_parent 			as 'parent',
-			posts.post_publishdate 		as 'publishdate',
-			posts.filter_id 			as 'filter_id',
-			posts.post_sarshomar		as 'sarshomar',
-			posts.date_modified  	    as 'date_modified',
-			IFNULL(posts.comment_count,0) ,
-			pollstats.id 		     	as 'pollstatsid',
-			IFNULL(pollstats.total,0)	as 'total'
+			posts.id						as 'id',
+			posts.post_language 			as 'language',
+			posts.post_title 				as 'title',
+			posts.post_slug 				as 'slug',
+			posts.post_url 					as 'url',
+			posts.post_content 				as 'content',
+			posts.post_type 				as 'type',
+			posts.post_comment 				as 'comment',
+			posts.post_meta 				as 'meta',
+			posts.post_count 				as 'count',
+			posts.post_order 				as 'order',
+			posts.post_status 				as 'status',
+			posts.post_parent 				as 'parent',
+			posts.post_publishdate 			as 'publishdate',
+			posts.filter_id 				as 'filter_id',
+			posts.post_sarshomar			as 'sarshomar',
+			posts.date_modified  	    	as 'date_modified',
+			IFNULL(posts.comment_count,0)   as 'comment_count',
+			pollstats.id 		     		as 'pollstatsid',
+			IFNULL(pollstats.total,0)		as 'total'
 		FROM
 			posts
 		LEFT JOIN pollstats ON pollstats.post_id = posts.id
