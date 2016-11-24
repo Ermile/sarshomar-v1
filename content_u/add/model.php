b6db1c752b20c9d575cde838a2f4ece79d3567db
Reza Mohiti <rm.biqarar@gmail.com>
2016-11-24 17:53:18 +0330

    update poll

--- content_u/add/model.php
+++ content_u/add/model.php
@@ -6,6 +6,21 @@ use \lib\debug;
 class model extends \content_u\home\model
 {
 
+
+	/**
+	 * update mod
+	 * @var        boolean
+	 */
+	private $update_mode = false;
+
+
+	/**
+	* the poll id
+	* @var        integer
+	*/
+	private $poll_id     = 0;
+
+
 	/**
 	 * Gets the edit.
 	 *
@@ -43,364 +58,45 @@ class model extends \content_u\home\model
 
 
 	/**
-	 * udate record of poll meta has saved in option table
-	 *
-	 * @param      <type>  $_meta_name  The meta name
-	 * @param      string  $_status     The status
-	 */
-	public function update_meta_in_option($_meta_name, $_status = 'enable')
-	{
-		// get poll meta in option table
-		$poll_meta = \lib\db\posts::get_post_meta($this->poll_id);
-		$option_id = null;
-		foreach ($poll_meta as $key => $value)
-		{
-			if($value['option_key'] == 'meta' && $value['option_value'] == $_meta_name)
-			{
-				$option_id = $value['id'];
-			}
-		}
-		if($option_id)
-		{
-			// update status of meta in option table
-			$update = ['option_status' => $_status];
-			\lib\db\options::update($update, $option_id);
-		}
-		elseif($_status == 'enable')
-		{
-			// insert new record in data in option_table
-			$insert_option =
-			[
-				'post_id'       => $this->poll_id,
-				'option_cat'    => "poll_{$this->poll_id}",
-				'option_key'    => 'meta',
-				'option_value'  => $_meta_name,
-				'option_status' => 'enable'
-			];
-			\lib\db\options::insert($insert_option);
-		}
-	}
-
-
-	/**
-	 * chekc value posted and update meta
+	 * remove all meta in option table of this poll
 	 *
 	 * @param      <type>  $_key    The key
 	 * @param      string  $_value  The value
 	 */
-	function poll_meta_update($_key, $_value)
+	function remove_meta()
 	{
-		$_key = str_replace('meta_', '', $_key);
-		if($_value == 'on')
-		{
-			if(isset($this->poll['meta'][$_key]) && $this->poll['meta'][$_key] == false)
-			{
-				\lib\db\polls::replace_meta([$_key => true], $this->poll_id);
-			}
-			else
-			{
-				\lib\db\polls::merge_meta([$_key => true], $this->poll_id);
-			}
-			$this->update_meta_in_option($_key, 'enable');
-		}
-		elseif($_value == '')
-		{
-			if(isset($this->poll['meta'][$_key]) && $this->poll['meta'][$_key] == true)
-			{
-				\lib\db\polls::replace_meta([$_key => false], $this->poll_id);
-				$this->update_meta_in_option($_key, 'disable');
+		$cat = "poll_". $this->poll_id;
+		$query =
+		"
+			DELETE FROM
+				options
+			WHERE
+			post_id = {$this->poll_id} AND
+			option_cat = '$cat' AND
+			option_key LIKE 'meta'
+		";
+		\lib\db::query($query);
 
-			}
-		}
-	}
-
-
-	/**
-	 * update the poll
-	 *
-	 * @param      <type>  $_args  The arguments
-	 */
-	function update_poll($_args)
-	{
-		// get the poll id
-		$poll_id = $this->check_poll_url($_args);
-		// save poll id in $this
-		$this->poll_id = $poll_id;
-		// get poll data
-		$poll    = \lib\db\polls::get_poll($poll_id);
-		// save poll data in $this
-		$this->poll = $poll;
-		// get the answrs data
-		$answers = \lib\utility\answers::get($poll_id);
-		// get the $_POST
-		$post    = utility::post();
-
-		// list of value to update
-		$update_post = [];
-
-		//check the words of post and set the status
-		if(!\lib\db\words::save_and_check($post))
-		{
-			$update_post['post_status'] = 'awaiting';
-		}
-
-		// check all value posted to this page and update if need
-		foreach ($post as $key => $value)
-		{
-			// decode the value posted
-			$value = html_entity_decode($value);
-
-			// swich title of post
-			switch ($key)
-			{
-					case 'title':
-						// check old title and new title
-						if($poll['title'] != $value)
-						{
-							// update the title
-							$update_post['post_title'] = $value;
-							$update_post['post_url']   = \lib\db\polls::update_url($poll_id, $value, false);
-							$update_post['post_slug']  = \lib\utility\filter::slug($value);
-						}
-						break;
-
-					case 'description':
-						// check the content of poll
-						if($poll['content'] != $value)
-						{
-							$update_post['post_content'] = $value;
-						}
-						break;
-
-					case 'poll_type':
-						// check the poll type
-						$type = self::change_type($value);
-						if($poll['type'] != $type)
-						{
-							$update_post['post_type'] = $type;
-						}
-						break;
-
-					case 'summary':
-						if(!isset($poll['meta']['desc']) || $poll['meta']['desc'] != $value)
-						{
-							\lib\db\polls::replace_meta(['desc' => $value], $poll_id);
-						}
-						break;
-
-					case 'sarshomar_knowledge':
-						if($this->access('u', 'sarshomar_knowledge', 'add'))
-						{
-							if($value == 'sarshomar')
-							{
-								if($poll['sarshomar'] == null)
-								{
-									$update_post['post_sarshomar'] = 1;
-								}
-							}
-							else
-							{
-								if($poll['sarshomar'] == '1')
-								{
-									$update_post['post_sarshomar'] = null;
-								}
-							}
-						}
-						break;
-
-					case 'meta_comment':
-						if($value == 'on')
-						{
-							if($poll['comment'] == 'close')
-							{
-								$update_post['post_comment'] = 'open';
-								\lib\db\polls::replace_meta(['comment' => true], $poll_id);
-								$this->update_meta_in_option('comment', 'enable');
-							}
-						}
-						elseif($value == '')
-						{
-							if($poll['comment'] == 'open')
-							{
-								$update_post['post_comment'] = 'close';
-								\lib\db\polls::replace_meta(['comment' => false], $poll_id);
-								$this->update_meta_in_option('comment', 'disable');
-							}
-						}
-						break;
-
-					case 'parent_tree_id':
-					case 'parent_tree_opt':
-						if(utility::post("parent_tree_opt") && utility::post("parent_tree_id"))
-						{
-							if(utility::post("parent_tree_id") == '')
-							{
-								\lib\utility\poll_tree::remove($poll_id);
-							}
-							else
-							{
-								$tree =
-								[
-									'parent' => utility::post("parent_tree_id"),
-									'child'  => $poll_id,
-									'opt'    => utility::post("parent_tree_opt")
-								];
-
-								\lib\utility\poll_tree::update($poll_id, $tree);
-							}
-						}
-						break;
-
-					// case 'meta_profile':
-					case 'meta_random_sort':
-					case 'meta_descriptive':
-					case 'meta_score':
-					case 'meta_hidden_result':
-						$this->poll_meta_update($key, $value);
-						break;
-
-					// update the answers
-					case substr($key, 0, 7) == 'answers':
-
-						$preg = preg_match("/(.*)\_(\d+)$/", $key, $ans);
-
-						if(!$preg)
-						{
-							break;
-						}
-
-						$type      = $ans[1];
-						$id        = intval($ans[2]);
-						$opt_index = $id - 1;
-
-						if($type != 'answers')
-						{
-							break;
-						}
-
-						// must update the
-						if(isset($answers[$opt_index]['option_value']))
-						{
-							$option_id = isset($answers[$opt_index]['id']) ? $answers[$opt_index]['id'] : 0;
-
-							if($answers[$opt_index]['option_value'] != $value && $value != '')
-							{
-								$answer_meta = [];
-
-								// if(isset($answers[$opt_index]['option_meta']['desc']))
-								// {
-								// 	if(utility::post("answer_desc_". $id) && utility::post("answer_desc_". $id != ''))
-								// 	{
-								// 		$answer_meta['desc'] = utility::post("answer_desc_". $id);
-								// 	}
-								// 	else
-								// 	{
-								// 		$answer_meta['desc'] = $answers[$opt_index]['option_meta']['desc'];
-								// 	}
-								// }
-
-								$update_answers =
-								[
-									'option_value' => $value,
-									// 'option_meta'  => json_encode($answer_meta, JSON_UNESCAPED_UNICODE)
-								];
-								\lib\db\options::update($update_answers, $option_id);
-							}
-							elseif ($value == '')
-							{
-								if(count($answers) > 2)
-								{
-									\lib\db\options::update(['option_status' => 'disable'], $option_id);
-								}
-							}
-						}
-						else
-						{
-							if($value != '')
-							{
-								$answer_meta          = [];
-								$answer_meta['type']  = utility::post("answer_type_". $id);
-								$answer_meta['point'] = utility::post("answer_point_". $id);
-								$answer_meta['true']  = utility::post("answer_true_". $id);
-								$answer_meta['desc']  = utility::post("answer_desc_". $id);
-								$answer_count = count($answers);
-								$answer_count++;
-								// must add new answer
-								$insert_answer =
-								[
-									'post_id'       => $poll_id,
-									'option_cat'    => "poll_$poll_id",
-									'option_key'    => "opt_$answer_count",
-									'option_value'  => $value,
-									'option_meta'   => json_encode($answer_meta, JSON_UNESCAPED_UNICODE),
-									'option_status' => 'enable'
-								];
-								\lib\db\options::insert($insert_answer);
-							}
-						}
-						break;
-					default:
-						# code...
-						break;
-				}
-		}
-		if(!empty($update_post))
-		{
-			$update_post = \lib\db\polls::update($update_post, $poll_id);
-		}
-
-		// muset be update post meta
-		\lib\db\polls::update_answer_in_meta($poll_id);
-
-		if(debug::$status)
-		{
-			debug::true(T_("all change saved"));
-			// get the url
-			$url = $this->check_poll_url($_args, "encode");
-			// must be redirect to filter page
-			if(utility::post("filter"))
-			{
-				$this->redirector()->set_url("@/add/$url/filter");
-			}
-			// must be redirect to publish page
-			elseif(utility::post("publish"))
-			{
-				$this->redirector()->set_url("@/add/$url/publish");
-			}
-		}
-		else
-		{
-			debug::error(T_("we can not save change on this poll"));
-		}
 	}
 
 
 	/**
 	 * Removes an answer.
 	 */
-	function remove_answer($_args)
+	function remove_answers()
 	{
-		$answer_id = utility::post("value");
+		$cat = "poll_". $this->poll_id;
+		$query =
+		"
+			DELETE FROM
+				options
+			WHERE
+			post_id = {$this->poll_id} AND
+			option_cat = '$cat' AND
+			option_key LIKE 'opt%'
+		";
+		\lib\db::query($query);
 
-		$poll_id = $this->check_poll_url($_args);
-		if(!$poll_id)
-		{
-			return true;
-		}
-		else
-		{
-			$answers = \lib\utility\answers::get($poll_id);
-			if(count($answers) <= 2 )
-			{
-				return false;
-			}
-			else
-			{
-				\lib\db\options::update(['option_status' => 'disable'], $answer_id);
-				\lib\db\polls::update_answer_in_meta($poll_id);
-				return true;
-			}
-		}
 	}
 
 
@@ -417,21 +113,21 @@ class model extends \content_u\home\model
 			return;
 		}
 
-		// remove one answer
-		// the user click on element by class .delete
-		if(utility::post("type") == 'remove_answer')
-		{
-			$this->remove_answer($_args);
-			return;
-		}
+		// // remove one answer
+		// // the user click on element by class .delete
+		// if(utility::post("type") == 'remove_answer')
+		// {
+		// 	$this->remove_answer($_args);
+		// 	return;
+		// }
 
 		/**
 		 * update the poll or survey
 		 */
 		if($this->check_poll_url($_args))
 		{
-			$this->update_poll($_args);
-			return;
+			$this->update_mode = true;
+			$this->poll_id = $this->check_poll_url($_args, "decode");
 		}
 
 		// check sarshoamr knowlege permission
@@ -534,18 +230,21 @@ class model extends \content_u\home\model
 				'post_status'    => 'draft',
 				'post_sarshomar' => $this->post_sarshomar
 			];
-			$survey_id = \lib\db\polls::insert($args);
-
-			// insert the poll
-			$insert_poll = $this->insert_poll(['survey_id' => $survey_id]);
-			// redirect to @/$url/add to add another poll
-			$url = \lib\utility\shortURL::encode($survey_id);
-			if($insert_poll)
+			if(!$this->update_mode)
 			{
-				// set dashboard data
-				\lib\utility\profiles::set_dashboard_data($this->login('id'), 'my_survey');
-				$this->redirector()->set_url("@/add/$url");
+				$survey_id = \lib\db\polls::insert($args);
+				// insert the poll
+				$insert_poll = $this->insert_poll(['survey_id' => $survey_id]);
+				// redirect to @/$url/add to add another poll
+				$url = \lib\utility\shortURL::encode($survey_id);
+				if($insert_poll)
+				{
+					// set dashboard data
+					\lib\utility\profiles::set_dashboard_data($this->login('id'), 'my_survey');
+					$this->redirector()->set_url("@/add/$url");
+				}
 			}
+
 
 		}
 		elseif(utility::post("add_poll"))
@@ -575,9 +274,15 @@ class model extends \content_u\home\model
 		// save poll tree
 		if(utility::post("parent_tree_id") && utility::post("parent_tree_opt"))
 		{
+
 			$loc_id  = utility::post("parent_tree_id");
 			if(is_numeric($loc_id))
 			{
+				if($this->update_mode)
+				{
+					\lib\utility\poll_tree::remove($this->poll_id);
+				}
+
 				$loc_opt = explode(',',utility::post("parent_tree_opt"));
 				foreach ($loc_opt as $key => $value)
 				{
@@ -718,10 +423,11 @@ class model extends \content_u\home\model
 	 */
 	function answers_in_post()
 	{
-		$answers      =[];
-		$answer_true  =[];
-		$answer_type  =[];
-		$answer_point =[];
+		$answers      = [];
+		$answer_true  = [];
+		$answer_type  = [];
+		$answer_point = [];
+		$answer_desc  = [];
 
 		foreach (utility::post() as $key => $value)
 		{
@@ -747,6 +453,10 @@ class model extends \content_u\home\model
 					case 'answer_point':
 						$answer_point[$id] = $value;
 						break;
+
+					case 'answer_desc':
+						$answer_desc[$id] = $value;
+						break;
 				}
 			}
 		}
@@ -757,6 +467,7 @@ class model extends \content_u\home\model
 			'answer_true'  => $answer_true,
 			'answer_type'  => $answer_type,
 			'answer_point' => $answer_point,
+			'answer_desc'  => $answer_desc,
 		];
 	}
 
@@ -846,13 +557,26 @@ class model extends \content_u\home\model
 			'post_sarshomar' => $this->post_sarshomar
 		];
 		// inset poll
-		$poll_id = \lib\db\polls::insert($args);
+		if(!$this->update_mode)
+		{
+			$poll_id = \lib\db\polls::insert($args);
+		}
+		else
+		{
+			\lib\db\polls::update($args, $this->poll_id);
+			$poll_id = $this->poll_id;
+		}
 
 		$answers_data = $this->answers_in_post();
 		$answers      = $answers_data['answers'];
 		$answer_type  = $answers_data['answer_type'];
 		$answer_true  = $answers_data['answer_true'];
 		$answer_point = $answers_data['answer_point'];
+
+		if($this->update_mode)
+		{
+			$this->remove_answers();
+		}
 
 		// check answers
 		if($answers)
@@ -890,6 +614,12 @@ class model extends \content_u\home\model
 			return false;
 		}
 		// get the metas of this poll
+
+		if($this->update_mode)
+		{
+			$this->remove_meta();
+		}
+
 		$metas       = [];
 		$post_meta   = [];
 		$insert_meta = false;
