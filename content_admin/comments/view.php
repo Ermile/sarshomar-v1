c35d7358184179de2cb424e1eb0296313d4ccaba
majix <majid.hfard@gmail.com>
2016-12-25 11:10:01 +0330

    Admin Area pages updated with a new design

--- content_admin/comments/view.php
+++ content_admin/comments/view.php
@@ -6,6 +6,8 @@ class view extends \mvc\view
 	function view_comments($_args)
 	{
 		$this->data->comments = $_args->api_callback;
+		// get fontawesome class
+		$this->include->fontawesome = true;
 	}
 }
 ?>
