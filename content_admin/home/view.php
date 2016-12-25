eb4ef27aee67044f0dc1c953f56c1e2ea4b760d8
Javad <J.Evazzadeh@live.com>
2016-12-25 11:23:32 +0330

    add chart of visitors to admin

--- content_admin/home/view.php
+++ content_admin/home/view.php
@@ -3,5 +3,10 @@ namespace content_admin\home;
 
 class view extends \mvc\view
 {
+	function config()
+	{
+		$this->include->chart = true;
+		$this->data->visitors = \lib\utility\visitor::chart();
+	}
 }
 ?>
