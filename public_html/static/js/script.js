92d7aee5c089d07e7ec13967e79a089bcccecb03
Javad <J.Evazzadeh@live.com>
2017-01-01 14:07:37 +0330

    remove heat test

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -155,14 +155,6 @@ function startCrop(_el)
 }
 
 
-function testHeatMap()
-{
-	(function(h,e,a,t,m,p) {
-	m=e.createElement(a);m.async=!0;m.src=t;
-	p=e.getElementsByTagName(a)[0];p.parentNode.insertBefore(m,p);
-	})(window,document,'script','https://u.heatmap.it/log.js');
-}
-
 /**
  * set link of language on each page
  */
@@ -203,7 +195,6 @@ route('*', function ()
 }).once(function()
 {
 	setLanguageURL();
-	testHeatMap();
 });
 
 
