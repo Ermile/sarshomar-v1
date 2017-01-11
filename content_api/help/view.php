48b179a54337ee9d919173476714ef5281855921
Reza <rm.biqarar@gmail.com>
2017-01-11 11:50:41 +0330

    api help matched

--- content_api/help/view.php
+++ content_api/help/view.php
@@ -6,7 +6,8 @@ class view extends  \mvc\view
 	public function config()
 	{
 		$this->data->template['poll_add'] = 'content_api/help/template/poll_add.html';
-
+		$this->data->template['urls']     = 'content_api/help/template/urls.html';
+		$this->data->template['sample']     = 'content_api/help/template/sample.html';
 	}
 }
 ?>
