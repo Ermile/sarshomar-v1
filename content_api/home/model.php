4b86d7ca6cbf4de4454c6a095ff4177d7198dc47
Reza <rm.biqarar@gmail.com>
2017-01-23 10:47:32 +0330

    api check user id

--- content_api/home/model.php
+++ content_api/home/model.php
@@ -9,17 +9,23 @@ class model extends \mvc\model
 	 */
 	public function permission()
 	{
+		if(!$this->user_id)
+		{
+			\lib\debug::error(T_("User not found"), 'access', 'user');
+			return;
+		}
+
 		$user_perm = \lib\db\users::get_user_data($this->user_id, 'user_permission');
 		if(isset($user_perm['user_permission']) && is_numeric($user_perm['user_permission']))
 		{
-			$permission                   = $this->setPermissionSession($user_perm['user_permission'], false);
 			$args                         = [];
 			$args['user']['permission']   = $user_perm['user_permission'];
-			$args['permission']           = $permission;
+			$args['permission']           = $this->setPermissionSession($user_perm['user_permission'], true);
 			permission::$get_from_session = false;
 			permission::$PERMISSION       = $args;
 		}
 	}
+
 	use tools\get_token;
 	use tools\ready;
 }
