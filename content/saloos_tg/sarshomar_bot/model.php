27465490c453a2a511601e18da747ef2c5976579
baravak <itb.baravak@gmail.com>
2017-01-25 15:24:07 +0330

    telegram model api

--- content/saloos_tg/sarshomar_bot/model.php
+++ content/saloos_tg/sarshomar_bot/model.php
@@ -1,8 +1,14 @@
 <?php
 namespace content\saloos_tg\sarshomar_bot;
-
+use \content_api\v1;
 class model extends \lib\mvc\model{
-	use \content_api\poll\tools\config;
-	use \content_api\poll\tools\post;
+	use \content_api\v1\home\tools\ready;
+	use \content_api\v1\poll\tools\add {
+		\content_api\v1\poll\tools\add::add as add_poll;
+	}
+
+	use \content_api\v1\poll\tools\get {
+		\content_api\v1\poll\tools\get::get as get_poll;
+	}
 }
 ?>
