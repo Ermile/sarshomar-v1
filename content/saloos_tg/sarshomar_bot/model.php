f46fad2e769125140fbf8c189a49b724baaeb54d
baravak <itb.baravak@gmail.com>
2017-01-31 19:20:28 +0330

    telegram model trait change

--- content/saloos_tg/sarshomar_bot/model.php
+++ content/saloos_tg/sarshomar_bot/model.php
@@ -3,14 +3,13 @@ namespace content\saloos_tg\sarshomar_bot;
 use \content_api\v1;
 class model extends \lib\mvc\model{
 	use \content_api\v1\home\tools\ready;
-	use \content_api\v1\poll\tools\add {
-		\content_api\v1\poll\tools\add::add as add_poll;
-	}
 
-	use \content_api\v1\poll\tools\get {
-		\content_api\v1\poll\tools\get::get as get_poll;
-	}
+	use \content_api\v1\poll\tools\add;
+
+	use \content_api\v1\poll\tools\get;
 
 	use \content_api\v1\poll\tools\delete;
+
+	use \content_api\v1\poll\search\tools\search;
 }
 ?>
