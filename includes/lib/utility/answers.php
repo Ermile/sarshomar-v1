69d2218452404f1f0e252493dee03a0a70de4472
Reza <rm.biqarar@gmail.com>
2017-02-10 08:29:21 +0330

    update answers bug fix

--- includes/lib/utility/answers.php
+++ includes/lib/utility/answers.php
@@ -6,6 +6,12 @@ use \lib\utility\shortURL;
 
 class answers
 {
+	public static $old_answer;
+	public static $must_insert;
+	public static $must_remove;
+
+
+
 	/**
 	 * check the user answered to this poll or no
 	 *
@@ -72,11 +78,11 @@ class answers
 				break;
 
 			case 'edit':
-				return self::check_access($_args, 'edit');
+				return self::check_time_count($_args, 'edit');
 				break;
 
 			case 'delete':
-				return self::check_access($_args, 'delete');
+				return self::check_time_count($_args, 'delete');
 				break;
 
 			case 'check':
@@ -88,12 +94,12 @@ class answers
 				}
 				else
 				{
-					if(self::check_access($_args, 'edit'))
+					if(self::check_time_count($_args, 'edit'))
 					{
 						array_push($avalible, 'edit');
 					}
 
-					if(self::check_access($_args, 'delete'))
+					if(self::check_time_count($_args, 'delete'))
 					{
 						array_push($avalible, 'delete');
 					}
@@ -229,7 +235,7 @@ class answers
 	 * @param      array   $_args  The arguments
 	 * @param      <type>  $_type  The type
 	 */
-	public static function check_access($_args = [], $_type)
+	public static function check_time_count($_args = [], $_type)
 	{
 		$default_args =
 		[
@@ -242,14 +248,16 @@ class answers
 			'subport' => null,
 			'debug'   => true,
 			'execute' => false,
+			'time'    => 60 * 100, // in dev mode
+			'count'   => 3 * 100, // in dev mode
 		];
 
 		$_args = array_merge($default_args, $_args);
 
 			// get the old answered user to this poll
-		$old_answer = \lib\db\polldetails::get($_args['user_id'], $_args['poll_id']);
+		self::$old_answer = \lib\db\polldetails::get($_args['user_id'], $_args['poll_id']);
 
-		if(!$old_answer || !is_array($old_answer))
+		if(!self::$old_answer || !is_array(self::$old_answer))
 		{
 			if($_type === 'edit' || $_type === 'delete')
 			{
@@ -261,10 +269,11 @@ class answers
 			}
 		}
 
-		$must_remove = [];
-		$must_insert = [];
+		$old_opt =  array_column(self::$old_answer, 'opt');
 
-		$old_opt =  array_column($old_answer, 'opt');
+		self::$must_remove = array_diff($old_opt, $_args['answer']);
+		self::$must_insert = array_diff($_args['answer'], $old_opt);
+
 		$new_opt = array_keys($_args['answer']);
 		if($old_opt == $new_opt)
 		{
@@ -276,39 +285,69 @@ class answers
 				}
 				return false;
 			}
-		}
-
-		return debug::error(T_("Duplicate answer, needless to update"), 'answer', 'permission');
-		var_dump($_args);
-		var_dump($old_opt);
-		var_dump($new_opt);
-		var_dump($old_answer);
-		exit();
-		// make a array similar the answer array
-		foreach ($opt_list as $key => $value)
-		{
-			if($value)
+			elseif($_type === 'delete')
 			{
-				$opt_list[$key] = (int) $value;
-			}
-			else
-			{
-				$opt_list[$key] = $value;
+				return true;
 			}
 		}
-		$must_remove = array_diff($opt_list, $_args['answer']);
-		$must_insert = array_diff($_args['answer'], $opt_list);
-		return [$must_remove, $must_insert, $old_answer];
-		switch ($_type)
-		{
-			case 'value':
-				# code...
-				break;
 
-			default:
-				# code...
-				break;
+		$insert_time = date("Y-m-d H:i:s");
+		$now         = time();
+		foreach (self::$old_answer as $key => $value)
+		{
+			if(isset($value['insertdate']))
+			{
+				$insert_time = $value['insertdate'];
+			}
+			break;
 		}
+
+		$insert_time  = strtotime($insert_time);
+		$diff_seconds = $now - $insert_time;
+
+		if($diff_seconds > $_args['time'])
+		{
+			if($_args['debug'])
+			{
+				debug::error(T_("Many time left, can not update answer"), 'answer', 'permission');
+			}
+			return false;
+		}
+
+		// get count of updated the poll
+		$where =
+		[
+			'post_id'    => $_args['poll_id'],
+			'user_id'    => $_args['user_id'],
+			'option_cat' => "update_user_$_args[user_id]",
+			'option_key' => "update_result_$_args[poll_id]",
+		];
+
+		if($_args['execute'])
+		{
+			\lib\db\options::plus($where);
+		}
+
+		$where['limit'] = 1;
+
+		$update_count = \lib\db\options::get($where);
+
+		if(!$update_count || !is_array($update_count) || !isset($update_count['value']))
+		{
+			return true;
+		}
+
+		$update_count = intval($update_count['value']);
+
+		if($update_count > $_args['count'])
+		{
+			if($_args['debug'])
+			{
+				debug::error(T_("Many update what are you doing?"),'answer', 'permission');
+			}
+			return false;
+		}
+		return true;
 	}
 
 
@@ -428,20 +467,21 @@ class answers
 		// when update the polldetails neet to update the pollstats
 		// on this process we check the old answer and new answer
 		// and update pollstats if need
-		if(!self::check_access($_args, 'edit'))
+		$_args['execute'] = true;
+		if(!self::check_time_count($_args, 'edit'))
 		{
 			return;
 		}
 
 		// remove answer must be remove
-		foreach ($must_remove as $key => $value)
+		foreach (self::$must_remove as $key => $value)
 		{
 			$remove_old_answer = \lib\db\polldetails::remove($_args['user_id'], $_args['poll_id'], $value);
 
 			$profile    = 0;
 			$validation = 'invalid';
 
-			foreach ($old_answer as $i => $o)
+			foreach (self::$old_answer as $i => $o)
 			{
 				if($o['opt'] == $value)
 				{
@@ -462,11 +502,36 @@ class answers
 			\lib\utility\stat_polls::set_poll_result($answers_details);
 		}
 
-		if(!empty($must_insert))
+		if(!empty(self::$must_insert))
 		{
-			$_args['options']['in_update']   = true;
-			$_args['options']['update_mode'] = true;
-			self::save($_args);
+			foreach ($_args['answer'] as $key => $value)
+			{
+				$set_option =
+				[
+					'answer_txt' => $value,
+					'validation' => $validation,
+					'port'       => $_args['port'],
+					'subport'    => $_args['subport'],
+				];
+
+				$result = \lib\db\polldetails::save($_args['user_id'], $_args['poll_id'], $key, $set_option);
+				// save the poll lucked by profile
+				// update users profile
+				$answers_details =
+				[
+					'validation'  => $validation,
+					'poll_id'     => $_args['poll_id'],
+					'opt_key'     => $key,
+					'user_id'     => $_args['user_id'],
+					'update_mode' => false,
+				];
+
+				\lib\utility\stat_polls::set_poll_result($answers_details);
+				// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				// $update_profile = \lib\utility\profiles::set_profile_by_poll($answers_details);
+				// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			}
+
 		}
 
 		return debug::true(T_("Your answer updated"));
