249d2bab52db4e2ffa63c4734d5190de36df339b
Reza <rm.biqarar@gmail.com>
2017-02-24 18:46:20 +0330

    add new permission list

--- content_admin/manifest.php
+++ content_admin/manifest.php
@@ -6,17 +6,7 @@ $modules = array();
 $modules['admin'] = array(
 	'desc' 			=> T_("Allow to show admin page"),
 	'icon'			=> 'file-text-o',
-	'permissions'	=> ['admin'],
+	'permissions'	=> ['view'],
 	);
-
-/**
- * accept and publish poll
- */
-$modules['publish_poll'] = array(
-	'desc' 			=> T_("Allow to change the post status to publish"),
-	'icon'			=> 'file-text-o',
-	'permissions'	=> ['admin'],
-	);
-
 return ["modules" => $modules];
 ?>
--- content_u/manifest.php
+++ content_u/manifest.php
@@ -1,5 +1,6 @@
 <?php
 $modules = array();
+
 /**
  * poll to complete profile
  */
@@ -8,6 +9,7 @@ $modules['complete_profile'] = array(
 	'icon'			=> 'file-text-o',
 	'permissions'	=> ['admin'],
 	);
+
 
 /**
  *	sarshomart knowledge
@@ -19,6 +21,7 @@ $modules['sarshomar_knowledge'] = array(
 	'permissions'	=> ['add', 'edit', 'view', 'update', 'admin'],
 	);
 
+
 /**
  * show hidden result checkbox
  */
@@ -28,5 +31,201 @@ $modules['hidden_result'] = array(
 	'permissions'	=> ['admin'],
 	);
 
+/**
+ * *********************************************************
+ * ****************** NEW PERMISSION LIST ******************
+ * *********************************************************
+ */
+
+/**
+ * free account
+ */
+$modules['free_account'] =
+[
+	'desc'        => T_("Use system whitout pay money"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+ * 0 add base money
+ */
+$modules['free_add_poll'] =
+[
+	'desc'        => T_("Allow to poll whitout pay money"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+ *
+* free_add_brand
+*/
+$modules['free_add_brand'] =
+[
+	'desc'        => T_("Allow to brand whitout pay money"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* free_add_filter
+*/
+$modules['free_add_filter'] =
+[
+	'desc'        => T_("Allow to add filter whitout pay money"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* free_add_member
+*/
+$modules['free_add_member'] =
+[
+	'desc'        => T_("Allow to member whitout pay money"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* lock_edit_mobile
+*/
+$modules['lock_edit_mobile'] =
+[
+	'desc'        => T_("Lock edti mobile"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* lock_edit_user_details
+*/
+$modules['lock_edit_user_details'] =
+[
+	'desc'        => T_("Lock edit user details"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* lock_edit_username
+*/
+$modules['lock_edit_username'] =
+[
+	'desc'        => T_("Lock edit username"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* sarshomar
+*/
+$modules['sarshomar'] =
+[
+	'desc'        => T_("Add poll to sarshomar knowledge"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* add_poll_cats
+*/
+$modules['add_poll_cats'] =
+[
+	'desc'        => T_("Allow to add poll cats"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* add_poll_article
+*/
+$modules['add_poll_article'] =
+[
+	'desc'        => T_("Allow to poll article"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* add_max_tags
+*/
+$modules['add_max_tags'] =
+[
+	'desc'        => T_("Allow to add 50 tags"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* max_update_answer
+*/
+$modules['max_update_answer'] =
+[
+	'desc'        => T_("Allow to update answer, for example the sarshomar user can update her meny times"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* draft_poll_10
+*/
+$modules['draft_poll_10'] =
+[
+	'desc'        => T_("Allow to set 10 draft poll"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* draft_poll_50
+*/
+$modules['draft_poll_50'] =
+[
+	'desc'        => T_("Allow to set 50 draft poll"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* draft_poll_500
+*/
+$modules['draft_poll_500'] =
+[
+	'desc'        => T_("Allow to set 500 draft poll"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* draft_poll_max
+*/
+$modules['draft_poll_max'] =
+[
+	'desc'        => T_("Allow to max poll draft"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* lock_answer_sarshomar_poll
+*/
+$modules['lock_answer_sarshomar_poll'] =
+[
+	'desc'        => T_("Lock answer to sarshomar poll"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
+
+/**
+* lock_answer_poll
+*/
+$modules['lock_answer_poll'] =
+[
+	'desc'        => T_("Lock answer to poll"),
+	'icon'        => 'file-text-o',
+	'permissions' => ['view'],
+];
 return ["modules" => $modules];
 ?>
