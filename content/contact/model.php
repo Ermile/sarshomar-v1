202ea575a348c0db031cbdd87e646cc3bec4f476
Reza <rm.biqarar@gmail.com>
2017-03-14 09:46:46 +0330

    save contact logs

--- content/contact/model.php
+++ content/contact/model.php
@@ -6,6 +6,14 @@ use \lib\utility;
 class model extends \mvc\model
 {
 
+	// log callers
+	// user:send:contact
+	// user:send:contact:fail
+	// user:send:contact:empty:message
+	// user:send:contact:empty:mobile
+	// user:send:contact:wrong:captha
+	// user:send:contact:register:by:mobile
+
 	/**
 	 * save contact form
 	 */
@@ -14,15 +22,24 @@ class model extends \mvc\model
 		// check login
 		if($this->login())
 		{
-			$user_id     = $this->login("id");
-			// get display name from user profile
+			$user_id = $this->login("id");
+
+			// get mobile from user login session
+			$mobile = $this->login('mobile');
+
+			if(!$mobile)
+			{
+				$mobile = utility::post('mobile');
+			}
+
+			// get display name from user login session
 			$displayname = $this->login("displayname");
 			// user not set users display name, we get display name from contact form
 			if(!$displayname)
 			{
 				$displayname = utility::post("name");
 			}
-			// get email from user profile
+			// get email from user login session
 			$email = \lib\db\users::get_email($user_id);
 			// user not set users email, we get email from contact form
 			if(!$email)
@@ -36,12 +53,47 @@ class model extends \mvc\model
 			$user_id     = null;
 			$displayname = utility::post("name");
 			$email       = utility::post("email");
+			$mobile      = utility::post("mobile");
 		}
 		// get the content
 		$content = utility::post("content");
+
+		// save log meta
+		$log_meta =
+		[
+			'meta' =>
+			[
+				'login'    => $this->login('all'),
+				'language' => \lib\define::get_language(),
+				'post'     => utility::post(),
+			]
+		];
+
+		/**
+		 * register user if set mobile and not register
+		 */
+		if($mobile && !$this->login())
+		{
+			// check valid mobile
+			if(\lib\utility\filter::mobile($mobile))
+			{
+				// check existing mobile
+				$exists_user = \lib\db\users::get_by_mobile($mobile);
+				// register if the mobile is valid
+				if(!$exists_user || empty($exists_user))
+				{
+					// signup user by site_guest
+					$user_id = \lib\db\users::signup(['mobile' => $mobile ,'type' => 'inspection', 'port' => 'site_guest']);
+					// save log by caller 'user:send:contact:register:by:mobile'
+					\lib\db\logs::set('user:send:contact:register:by:mobile', $user_id, $log_meta);
+				}
+			}
+		}
+
 		// check content
 		if($content == '')
 		{
+			\lib\db\logs::set('user:send:contact:empty:message', $user_id, $log_meta);
 			debug::error(T_("Please fill in the field"), "content");
 			return false;
 		}
@@ -58,10 +110,12 @@ class model extends \mvc\model
 		$result = \lib\db\comments::insert($args);
 		if($result)
 		{
+			\lib\db\logs::set('user:send:contact', $user_id, $log_meta);
 			debug::true(T_("Thank You For contacting us"));
 		}
 		else
 		{
+			\lib\db\logs::set('user:send:contact:fail', $user_id, $log_meta);
 			debug::error(T_("We could'nt save the contact"));
 		}
 	}
