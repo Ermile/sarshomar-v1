95aed9ee6796b4a8d54c5c9cb85040361b9c022d
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-03-22 15:48:50 +0430

    remove upgrades

--- includes/cls/database/upgrade/(db_name)/v.1.7.0_polldetails_change_enum.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `polldetails` CHANGE `validstatus` `validstatus` ENUM('valid','invalid') CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
--- includes/cls/database/upgrade/(db_name)/v.1.7.1_userpass_setnull.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `users` CHANGE `user_pass` `user_pass` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_bin NULL;
-
