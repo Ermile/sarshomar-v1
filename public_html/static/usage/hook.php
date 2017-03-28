6ed5c629863094c7e33be9ef4bae0d3452efd0f5
baravak <itb.baravak@gmail.com>
2017-03-28 20:35:57 +0430

    telegram hook

--- public_html/static/usage/hook.php
+++ public_html/static/usage/hook.php
@@ -1,5 +1,5 @@
 <?php
 $exec = exec('curl -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: ae91638b-0ec4-b416-6a61-5030980be481" "https://api.telegram.org/bot142711391:AAFH0ULw7BzwdmmiZHv2thKQj7ibb49DJ44/getWebhookinfo"');
 $x = json_decode($exec, true);
-var_dump($x['result']['pending_update_count']);
+echo "<font color='#4e9a06'>" . $x['result']['pending_update_count'] . "</font>";
 ?>
