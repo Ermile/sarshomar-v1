83356a60fb3560e52bb35327e95265cd3a78ab6d
baravak <itb.baravak@gmail.com>
2017-03-28 11:29:35 +0430

    telegram bugfix

--- content/saloos_tg/sarshomarbot/commands/inline_query.php
+++ content/saloos_tg/sarshomarbot/commands/inline_query.php
@@ -195,6 +195,8 @@ class inline_query
 				if(isset($row_result['mime_type']) && $row_result['mime_type'] = "video/mp4")
 				{
 					$row_result['type'] = 'video';
+					$row_result['document_url'] = $row_result['video_url'];
+					unset($row_result['document_url']);
 				}
 
 				unset($row_result['_url']);
