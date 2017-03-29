620e24c99689c0b2ea4d933c3a880c1a351638ae
reza <rm.biqarar@gmail.com>
2017-03-29 22:42:51 +0430

    show today data in admin chart

--- content_admin/home/tools/charts.php
+++ content_admin/home/tools/charts.php
@@ -10,14 +10,14 @@ trait charts
 	 */
 	public function chart_polls()
 	{
-		$start    = date("Y-m-d", time());
+		// $start    = date("Y-m-d", time());
 		$end      = date("Y-m-d", strtotime('-30 days'));
 		$language = \lib\define::get_language();
 
 		$poll_day_chart =
 		"SELECT DATE(posts.post_createdate) AS `createdate`, COUNT(posts.id) AS `count`
 		FROM posts
-		WHERE posts.post_createdate <= '$start' AND posts.post_createdate >= '$end'
+		WHERE  posts.post_createdate >= '$end'
 		GROUP BY DATE(posts.post_createdate) ORDER BY DATE(posts.post_createdate) DESC";
 
 		$poll_day_chart = \lib\db::get($poll_day_chart, ['createdate', 'count']);
@@ -46,14 +46,14 @@ trait charts
 	 */
 	public function chart_users_week()
 	{
-		$start    = date("Y-m-d", time());
+		// $start    = date("Y-m-d", time());
 		$end      = date("Y-m-d", strtotime('-100 days'));
 		$language = \lib\define::get_language();
 
 		$poll_day_chart =
 		"SELECT WEEK(users.user_createdate) AS `createdate`, COUNT(users.id) AS `count`
 		FROM users
-		WHERE users.user_createdate <= '$start' AND users.user_createdate >= '$end'
+		WHERE  users.user_createdate >= '$end'
 		GROUP BY WEEK(users.user_createdate) ORDER BY WEEK(users.user_createdate) DESC";
 
 		$poll_day_chart = \lib\db::get($poll_day_chart, ['createdate', 'count']);
@@ -77,14 +77,14 @@ trait charts
 	 */
 	public function chart_users()
 	{
-		$start    = date("Y-m-d", time());
+		// $start    = date("Y-m-d", time());
 		$end      = date("Y-m-d", strtotime('-30 days'));
 		$language = \lib\define::get_language();
 
 		$poll_day_chart =
 		"SELECT DATE(users.user_createdate) AS `createdate`, COUNT(users.id) AS `count`
 		FROM users
-		WHERE users.user_createdate <= '$start' AND users.user_createdate >= '$end'
+		WHERE  users.user_createdate >= '$end'
 		GROUP BY DATE(users.user_createdate) ORDER BY DATE(users.user_createdate) DESC";
 
 		$poll_day_chart = \lib\db::get($poll_day_chart, ['createdate', 'count']);
@@ -144,14 +144,14 @@ trait charts
 	 */
 	public function chart_answer()
 	{
-		$start    = date("Y-m-d", time());
+		// $start    = date("Y-m-d", time());
 		$end      = date("Y-m-d", strtotime('-30 days'));
 		$language = \lib\define::get_language();
 
 		$poll_day_chart =
 		"SELECT DATE(polldetails.insertdate) AS `createdate`, COUNT(polldetails.id) AS `count`
 		FROM polldetails
-		WHERE polldetails.insertdate <= '$start' AND polldetails.insertdate >= '$end'
+		WHERE  polldetails.insertdate >= '$end'
 		GROUP BY DATE(polldetails.insertdate) ORDER BY DATE(polldetails.insertdate) DESC";
 
 		$poll_day_chart = \lib\db::get($poll_day_chart, ['createdate', 'count']);
@@ -179,14 +179,14 @@ trait charts
 	 */
 	public function chart_transaction()
 	{
-		$start    = date("Y-m-d", time());
+		// $start    = date("Y-m-d", time());
 		$end      = date("Y-m-d", strtotime('-30 days'));
 		$language = \lib\define::get_language();
 
 		$poll_day_chart =
 		"SELECT DATE(transactions.createdate) AS `createdate`, COUNT(transactions.id) AS `count`
 		FROM transactions
-		WHERE transactions.createdate <= '$start' AND transactions.createdate >= '$end'
+		WHERE  transactions.createdate >= '$end'
 		GROUP BY DATE(transactions.createdate) ORDER BY DATE(transactions.createdate) DESC";
 
 		$poll_day_chart = \lib\db::get($poll_day_chart, ['createdate', 'count']);
