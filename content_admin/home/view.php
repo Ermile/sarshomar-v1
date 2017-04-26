7f88e488adb4d9bdb99ff41dce71a8982cb24ae6
Reza <rm.biqarar@gmail.com>
2017-04-26 14:22:56 +0430

    admin home page bug fix

--- content_admin/home/view.php
+++ content_admin/home/view.php
@@ -6,7 +6,7 @@ class view extends \mvc\view
 	function config()
 	{
 
-		$this->data->visitors = \lib\utility\visitor::chart();
+		// $this->data->visitors = \lib\utility\visitor::chart();
 
 		$this->show_chart();
 	}
@@ -29,6 +29,7 @@ class view extends \mvc\view
 		$this->data->chart_polls   = json_encode($this->model()->chart_polls(),JSON_UNESCAPED_UNICODE);
 		$this->data->chart_visitor = json_encode($this->model()->chart_visitor(),JSON_UNESCAPED_UNICODE);
 		$this->data->chart_answer  = json_encode($this->model()->chart_answer(),JSON_UNESCAPED_UNICODE);
+
 		if(\lib\utility::get('signup') == 'week')
 		{
 			$this->data->chart_users   = json_encode($this->model()->chart_users_week(),JSON_UNESCAPED_UNICODE);
