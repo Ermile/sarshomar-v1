08930c84bfe472d1d5dd5106791472a769dcfb27
Reza <rm.biqarar@gmail.com>
2017-05-04 16:25:27 +0430

    show active election in home page

--- content_election/home/view.php
+++ content_election/home/view.php
@@ -3,6 +3,32 @@ namespace content_election\home;
 
 class view extends \content_election\main\view
 {
+
+	/**
+	 * { function_description }
+	 */
+	public function config()
+	{
+		$election = \content_election\lib\elections::search();
+		$this->data->election_list = $election;
+		$running = [];
+		foreach ($election as $key => $value)
+		{
+			if(isset($value['status']) && $value['status'] === 'running')
+			{
+				$running[] = $value;
+			}
+		}
+
+		$this->data->running = $running;
+	}
+
+
+	/**
+	 * { function_description }
+	 *
+	 * @param      <type>  $_args  The arguments
+	 */
 	public function view_load($_args)
 	{
 		$result = $_args->api_callback;
