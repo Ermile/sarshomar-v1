405659a1307dfb6a4c3c0caf10b5ce06924d742c
Reza <rm.biqarar@gmail.com>
2017-05-04 16:37:32 +0430

    show percent of every candidas

--- content_election/home/model.php
+++ content_election/home/model.php
@@ -27,9 +27,32 @@ class model extends \content_election\main\model
 		if($election_id)
 		{
 			$result             = [];
-			$result['election'] = \content_election\lib\elections::get($election_id);
+			$election           = \content_election\lib\elections::get($election_id);
+			$result['election'] = $election;
 			$vote               = \content_election\lib\results::get_last($election_id);
+			$voted              = 0;
+
+			if(isset($election['voted']))
+			{
+				$voted += (int) $election['voted'];
+			}
+
+			if(isset($election['invalid']))
+			{
+				$voted += (int) $election['invalid'];
+			}
+
+			if(!$voted)
+			{
+				$voted = 1;
+			}
+
+			foreach ($vote as $key => $value)
+			{
+				$vote[$key]['percent'] = round((intval($value['total']) * 100) / $voted, 3);
+			}
 			$result['result']   = $vote;
+
 			return $result;
 		}
 	}
