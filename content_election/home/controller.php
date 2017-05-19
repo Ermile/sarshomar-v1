21a37978751a35157fdccb62bd0cff2f172bd9f3
Reza <rm.biqarar@gmail.com>
2017-05-19 18:35:20 +0430

    redirect election to iran/president

--- content_election/home/controller.php
+++ content_election/home/controller.php
@@ -8,13 +8,33 @@ class controller extends \content_election\main\controller
 		parent::_route();
 
 		$url = \lib\router::get_url();
+
 		if($id = $this->model()->check_url($url))
 		{
 			$this->get("load", "load")->ALL("/.*/");
 		}
 		else
 		{
-			$this->get('home', 'home')->ALL();
+			switch ($url)
+			{
+				case 'election':
+				case 'انتخابات':
+				case 'انتخاب':
+				case '':
+					$this->redirector($this->url('base'). '/election/iran')->redirect();
+					return;
+					# code...
+					break;
+				case 'iran':
+					$this->redirector($this->url('base'). '/election/iran/president')->redirect();
+					return;
+					break;
+				default:
+					# code...
+					break;
+			}
+
+			$this->get('home', 'home')->ALL("/iran\/president/");
 			$this->display_name = 'content_election\home\home.html';
 		}
 	}
