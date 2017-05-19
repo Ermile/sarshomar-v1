5567e140bd342ada589c1dc80b04c03383ff9fb0
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-05-19 23:12:55 +0430

    fix links and iamges of pages

--- content_election/home/view.php
+++ content_election/home/view.php
@@ -12,6 +12,9 @@ class view extends \content_election\main\view
 		$running  = [];
 		$election = \content_election\lib\elections::search();
 
+		$this->data->page['title']   = T_('Iranian president election result');
+		$this->data->page['desc']    = T_('Live and complete result of iran election after revolution until now.'). ' '. T_('Live result of iran 12 president election');
+
 		$this->data->election_list = $election;
 
 		foreach ($election as $key => $value)
@@ -45,6 +48,34 @@ class view extends \content_election\main\view
 	{
 		$result = $_args->api_callback;
 		$this->data->result = $result;
+
+		if($this->data->site['currentlang'] == 'fa')
+		{
+			if(isset($this->data->result['election']['title']))
+			{
+				$title_of_el = $this->data->result['election']['title'];
+				$this->data->page['title'] = $title_of_el;
+				$this->data->page['desc'] = 'نتایج لحظه‌ای '. $title_of_el. '. آخرین نتایج انتخابات ریاست جمهوری را بررسی کنید';
+			}
+		}
+		else
+		{
+			if(isset($this->data->result['election']['en_title']))
+			{
+				$title_of_el = $this->data->result['election']['en_title'];
+				$this->data->page['title'] = $title_of_el;
+				$this->data->page['desc'] = T_('Live result of '). $title_of_el;
+			}
+		}
+
+		if(isset($this->data->result['candida'][0]['file_url']))
+		{
+			$image_of_winner = $this->url('root'). $this->data->result['candida'][0]['file_url'];
+
+			$this->data->share['twitterCard'] = 'summary_large_image';
+			$this->data->share['image']       = $image_of_winner;
+		}
+		// var_dump($this->data->result);
 	}
 
 
